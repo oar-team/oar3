ac69552ba2ba68f77d41bd4237e39ab5923f976c
Olivier Richard <olivier.richard@imag.fr>
2014-12-01 09:16:10 +0100

    fix ResourceSet

--- kao/kao.py
+++ kao/kao.py
@@ -1,16 +1,16 @@
 import os
 import os.path as op
-import click
+#import click
 
 HERE = op.dirname(__file__)
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name', help='The person to greet.')
-def kao(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)                                        
+#@click.command()
+#@click.option('--count', default=1, help='Number of greetings.')
+#@click.option('--name', prompt='Your name', help='The person to greet.')
+#def kao(count, name):
+#    """Simple program that greets NAME for a total of COUNT times."""
+#    for x in range(count):
+#        click.echo('Hello %s!' % name)                                        
 
 if __name__ == '__main__':
     kao()
--- kao/resource.py
+++ kao/resource.py
@@ -1,10 +1,9 @@
 from oar import config, Resource
 from hierarchy import Hierarchy
-from intervals import ordered_ids2itvs
+from interval import ordered_ids2itvs
+from array import *
 
 MAX_NB_RESOURCES = 100000
-
-for array import *
 
 class ResourceSet:
     
@@ -18,7 +17,12 @@ class ResourceSet:
         #prepare hierachy stuff
         #"SCHEDULER_PRIORITY_HIERARCHY_ORDER="/host/cpu/core/"
         conf_hy_ordered_labels = config["SCHEDULER_PRIORITY_HIERARCHY_ORDER"]
-        hy_ordered_labels = [i for i in conf_hy_ordered_labels.split("/") if i != '' ]
+        hy_ordered_labels = []
+        for i in conf_hy_ordered_labels.split("/"):
+            if i != "":
+                if i == "resource_id":
+                    i = "id"
+                hy_ordered_labels.append(i)
 
         hy_rid = {}
         for hy_label in hy_ordered_labels:
@@ -29,35 +33,35 @@ class ResourceSet:
         available_upto = {}
         self.available_upto = {}
 
-
         roids = [] 
 
         #retreive resource in order from DB
         self.resources_db = Resource.query.order_by(order_by_clause).all()
 
         #fill the different structures
-        for roid, r in enumarate(self.resources_db):
+        for roid, r in enumerate(self.resources_db):
             if (r.state == "Alive") or (r.state == "Absent"):
-                roid = int(r.id)
-                roids.append(riod)
+                rid = int(r.id)
+                roids.append(roid)
                 self.rid_i2o[rid] = roid
-                self.rid_o2i[oid] = roid
+                self.rid_o2i[roid] = rid
 
                 #fill hy_rid structure
                 for hy_label in hy_ordered_labels:
                     v = getattr(r,hy_label)
                     if v:
-                        if hy_rid[hy_label][v]:
+                        if hy_label not in hy_rid:
+                            hy_rid[hy_label] = {}
+                        if v in hy_rid[hy_label]:
                             hy_rid[hy_label][v].append(roid)
                         else:
-                            hy_rid[hy_label][v] = []
+                            hy_rid[hy_label][v] = [roid]
 
                 #fill available_upto structure
-                if available_upto[r.available_upto]:
-                    available_upto[r.available_upto].append(rid)
+                if r.available_upto in available_upto:
+                    available_upto[r.available_upto].append(rid)            
                 else:
                     available_upto[r.available_upto] = [rid]
-            
             #global ordered resources intervals
             self.rid_itvs = ordered_ids2itvs(roids)
 
