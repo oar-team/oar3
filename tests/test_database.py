c0e7acae93057728c638d7b47abe1d16f2067313
Salem Harrache <salem.harrache@inria.fr>
2014-12-09 19:32:52 +0100

    Began test writing with sqlite

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
-from oar import db, Resource
+import pytest
+
+from sqlalchemy.orm.util import object_state
+
+from . import DEFAULT_CONFIG
+
+from oar import Resource
+from oar import config, db as initial_db
 
 
-def test_db_query():
-    db.reflect()
-    db.add(Resource(id=100))
-    db.add(Resource(id=200))
+@pytest.fixture(scope="session")
+def db(request):
+    config.clear()
+    config.update(DEFAULT_CONFIG)
+    def teardown():
+        initial_db.close()
+    initial_db.create_all()
+    initial_db.reflect()
+    request.addfinalizer(teardown)
+    return initial_db
+
+
+def test_db_insert(db):
+    r1 = Resource(id=100)
+    db.add(r1)
+    assert object_state(r1).pending is True
     db.flush()
+    assert object_state(r1).persistent is True
+
+
+def test_db_query(db):
     res = db.query(Resource).order_by(Resource.id.asc()).first()
-    assert res.id == 1
+    assert res.id == 100
