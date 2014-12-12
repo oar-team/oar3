27731c0c8dbd54e97cc344d5df408d7991ee33ad
Olivier Richard <olivier.richard@imag.fr>
2014-12-12 10:21:02 +0100

    wip: simulation

--- kao/simsim.py
+++ kao/simsim.py
@@ -1,3 +1,4 @@
+import collections
 import kamelot
 from platform import Platform
 from random import seed, randint
@@ -6,20 +7,25 @@ import simpy
 from simpy.events import AnyOf
 
 class SimSched:
-    def __init__(self, env):
-        self.env = env
+    def __init__(self, env, plt, jobs, submission_time_jids):
+        #self.env = env
+
+        self.platform = plt
+        self.jobs = jobs
+        self.sub_time_jids = submission_time_jids
+        self.sub_time_idx = 0
+        self.sub_time_len = len(submission_time_jids)
+
         self.sched_proc = env.process(self.sched(env))
-        self.new_job_id = 0
+
         self.evt_running_jobs = Set()
         self.evt_2_job = {}
-        self.waiting_jobs = []
+        self.waiting_jobs = Set()
 
     def sched(self, env):
         
         next_job_arrival = self.job_arrival(env)
 
-        i = 1
-        new_jobs = []
         while True:
 
             print 'Wait for job arrivals or job endings', env.now
@@ -35,7 +41,8 @@ class SimSched:
               print "event:..... ", k
               if k == next_job_arrival:
                   print "job arrives !", v
-                  self.waiting_jobs.extend(v)
+                  for jid in v:
+                      self.waiting_jobs.add(v)
                   next_job_arrival = self.job_arrival(env)
                   
               else:
@@ -58,19 +65,22 @@ class SimSched:
                 self.evt_2_job[evt_running_job] = j_to_launch
 
     def job_arrival(self, env):
-        #TODO generate from SWF trace
-        self.new_job_id += 1
-        new_job_ids = [self.new_job_id]
-        print new_job_ids
-        return env.timeout(randint(5,15),  new_job_ids)
+        if self.sub_time_idx < self.sub_time_len:
+            t, jids = self.sub_time_jids[self.sub_time_idx]
+            self.sub_time_idx += 1
+            return env.timeout(t, jids)
+        else:
+            return None
+
+        #self.new_job_id += 1
+        #new_job_ids = [self.new_job_id]
+        #print new_job_ids
+        #return env.timeout(randint(5,15),  new_job_ids)
  
 class ResourceSetSimu():
     def __init__(self, **kwargs):
-        self.rid_i2o = kwargs["rid_i2o"]
-        self.rid_o2i = kwargs["rid_o2i"]
-        self.hierarchy = kwargs["hierarchy"]
-        self.available_upto =  kwargs["available_upto"]
-        self.roid_itvs =  kwargs["roid_itvs"]
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
         
 class JobSimu():
     def __init__(self, **kwargs):
@@ -79,11 +89,36 @@ class JobSimu():
 
 env = simpy.Environment()
 nb_res = 10
+
+#
+# generate ResourceSet
+#
 res_set = ResourceSetSimu(
     rid_i2o = range(nb_res),
     rid_o2i = range(nb_res),
     roid_itvs = [(0,nb_res-1)]
 )
+
+#
+# generate jobs
+#
+
+nb_jobs = 10
+jobs = {}
+submission_time_jids = []
+
+for i in range(1,nb_jobs + 1):
+    jobs[i] = JobSimu( id = i,
+                       state = "Wainting",
+                       start_time = 0,
+                       types = {},
+                       res_set = [],
+                       moldable_id = 0,
+                       mld_res_rqts = [],
+                       )
+    
+    submission_time_jids.append( (10, [i]) )
+
 plt = Platform("simu", env=env, resource_set = res_set )
-simsched = SimSched(env, plt)
+simsched = SimSched(env, plt, jobs, submission_time_jids)
 env.run(until=40)
