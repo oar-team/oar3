f44ea6012af1b835890ee88ac475c2a9674c6cb0
Salem Harrache <salem.harrache@inria.fr>
2015-01-22 18:24:03 +0100

    Syncronized postgresql/mysql databases

--- oar_utils/archive.py
+++ oar_utils/archive.py
@@ -6,15 +6,13 @@ from sqlalchemy.sql.expression import select
 from sqlalchemy import func
 from oar.lib import config, db, Database
 from oar.utils import VERSION
-
-from sqlalchemy.engine import reflection
-from sqlalchemy.schema import (MetaData, Table, ForeignKeyConstraint, 
-                               DropConstraint)
+from oar.lib.models import *
 
 
-green = lambda x: click.style(x, fg="green")
-red = lambda x: click.style(x, fg="red")
-blue = lambda x: click.style(x, fg="blue")
+
+blue = lambda x: click.style("%s" % x, fg="blue")
+green = lambda x: click.style("%s" % x, fg="green")
+red = lambda x: click.style("%s" % x, fg="red")
 
 
 def log(msg, *args):
@@ -27,51 +25,29 @@ def log(msg, *args):
 def sync(db_url, db_archive_url, chunk_size=1000):
     db_archive = Database(uri=db_archive_url)
     create_all_tables(db_archive)
-    removed_foreign_keys(db_archive)
     sync_data(db_archive, chunk_size)
 
 
 def create_all_tables(db_archive):
-    log('Pulling schema from source server')
     for table in db.metadata.sorted_tables:
-        log("Creating %s", green(table.name))
-        table.create(db_archive.engine, checkfirst=True)
-
-
-def removed_foreign_keys(db_archive):
-    log("Dropping all foreign key constraints from archive database")
-
-    inspector = reflection.Inspector.from_engine(db_archive.engine)
-    fake_metadata = MetaData()
-
-    fake_tables = []
-    all_fks = []
-
-    for table_name in db_archive.metadata.tables:
-        fks = []
-        for fk in inspector.get_foreign_keys(table_name):
-            if fk['name']:
-                fks.append(ForeignKeyConstraint((),(),name=fk['name']))
-        t = Table(table_name, fake_metadata, *fks)
-        fake_tables.append(t)
-        all_fks.extend(fks)
-
-    connection = db_archive.engine.connect()
-    transaction = connection.begin()
-    for fkc in all_fks:
-        connection.execute(DropConstraint(fkc))
-    transaction.commit()
+        log(' %s | Creating %s', green('schema'), table.name)
+        table.create(bind=db_archive.engine, checkfirst=True)
 
 
 def sync_data(db_archive, chunk_size):
+
+    def logging(lenght, table_name):
+        message = '   %s | Copying %s record(s) from %s'
+        log(message, green('data'), blue(lenght), table_name)
+
     for table in db.metadata.sorted_tables:
-        insert_query = table.insert()
-        select_query = select([table])
         lenght = db.session.execute(select([func.count()])\
                            .select_from(table))\
                            .scalar()
+        insert_query = table.insert()
+        select_query = select([table])
         result = db.session.execute(select_query)
-        log("Synchronizing table %s - %d record(s)", table.name, lenght)
+        logging(lenght, table.name)
         while True:
             rows = result.fetchmany(chunk_size)
             if len(rows) == 0:
