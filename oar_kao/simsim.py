0f6b72ee86bc0977218cde051bdf7e42a2756852
Olivier Richard <olivier.richard@imag.fr>
2015-01-24 20:24:46 +0100

    wip: SWF Workload support

--- oar_kao/simsim.py
+++ oar_kao/simsim.py
@@ -106,4 +106,3 @@ class JobSimu():
     def __init__(self, **kwargs):
         for key, value in kwargs.iteritems():
             setattr(self, key, value)
-
---
+++ simu/sim2.py
@@ -0,0 +1,5 @@
+from oar.kao.simsim import ResourceSetSimu, JobSimu, SimSched, gene_jobsim_sub_tim
+
+swf_wkld = SWFWorkload("gofree_sample.swf")
+
+print swf_wkld.gene_jobsim_sub_time(100, 500, 1000)
