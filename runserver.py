2dd21d433901b0e85c61f54560ce83a9a798ae28
Salem Harrache <salem@harrache.info>
2015-02-02 18:48:58 +0100

    Enable debugger by default

--- runserver.py
+++ runserver.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import division, unicode_literals
-import os
+
 import argparse
 
-from oar.rest_api.views import app
+from oar.rest_api.app import create_app
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run application ')
-    parser.add_argument('-p', '--port', action="store", default=7001, type=int,
+    parser.add_argument('-p', '--port', action="store", default=9090, type=int,
                         help='Set the listening port')
     parser.add_argument('-b', '--bind', action="store", default="0.0.0.0",
                         help='Set the binding address')
 
     args = parser.parse_args()
-    app.run(host=args.bind, port=args.port, threaded=True)
+    app = create_app()
+    app.run(host=args.bind, port=args.port, threaded=True, debug=True)
