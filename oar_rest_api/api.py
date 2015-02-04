36584dab5cff91f12c91c7472bc45321e8228c0d
Salem Harrache <salem@harrache.info>
2015-02-04 15:35:05 +0100

    Added paginate query method

--- oar_rest_api/api.py
+++ oar_rest_api/api.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from functools import wraps
 
+from collections import OrderedDict
 import json
-
-from flask import Blueprint, Response
-
-from oar.lib import db
+from math import ceil
+import sys
 
 from .utils import JSONEncoder
+from flask import Blueprint, Response, abort, current_app, request, url_for
 from oar.lib.compat import reraise, to_unicode, iteritems
 from oar.lib.database import BaseQuery, BaseModel
 
@@ -148,6 +148,71 @@ class APIBaseQuery(BaseQuery):
         except:
             abort(404)
 
+    def paginate(self, offset, limit, error_out=True):
+        if limit is None:
+            limit = current_app.config.get("API_DEFAULT_MAX_ITEMS_NUMBER")
+        if error_out and offset < 0:
+            abort(404)
+        items = self.limit(limit).offset(offset).all()
+        if not items and offset != 0 and error_out:
+            abort(404)
+        # No need to count if we're on the first page and there are fewer
+        # items than we expected.
+        if offset == 0 and len(items) < limit:
+            total = len(items)
+        else:
+            total = self.order_by(None).count()
+        return Pagination(offset, limit, total, items)
+
 
 class APIBaseModel(BaseModel):
     pass
+
+
+class Pagination(object):
+    """Internal helper class returned by :meth:`APIBaseQuery.paginate`."""
+
+    def __init__(self, offset, limit, total, items):
+        self.offset = offset
+        self.limit = limit or 0
+        self.total = total
+        self.items = items
+
+    @property
+    def current_page(self):
+        """The number of the current page (1 indexed)"""
+        if self.limit > 0:
+            return int(ceil(self.offset / float(self.limit)))
+        return 1
+
+    @property
+    def pages(self):
+        """The total number of pages"""
+        if self.limit > 0:
+            return int(ceil(self.total / float(self.limit)))
+        return 1
+
+    @property
+    def has_next(self):
+        """True if a next page exists."""
+        return self.current_page < self.pages
+
+    @property
+    def next_url(self):
+        if self.has_next:
+            kwargs = {'offset': self.offset + self.limit, 'limit': self.limit}
+            return url_for(request.endpoint, **kwargs)
+
+    @property
+    def url(self):
+        kwargs = {'offset': self.offset}
+        if self.limit > 0:
+            kwargs['limit'] = self.limit
+        return url_for(request.endpoint, **kwargs)
+
+    def __iter__(self):
+        for item in self.items:
+            result = OrderedDict()
+            for key in item.keys():
+                result[key] = getattr(item, key)
+            yield result
