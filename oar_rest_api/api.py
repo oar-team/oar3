a6906b871f46185e05499c263949fae71a2c3eb3
Salem Harrache <salem@harrache.info>
2015-02-04 15:28:35 +0100

    Added args decorator to validate user input args (querystring, json, form)
    Example usage with:
    
    ::
    
        @api.route('/sum', methods=['get', 'post'])
        @api.args({'a': int, 'b': int})
        def sum(a=0, b=0):
            return {'total': a + b}

--- oar_rest_api/api.py
+++ oar_rest_api/api.py
@@ -37,6 +37,29 @@ class API(Blueprint):
             parent_method(rule + ".json", **options)(decorated)
         return decorator
 
+    def args(self, argmap, targets=None):
+        """Decorator that injects parsed arguments into a view function or
+        method.
+
+        Example usage with: ::
+
+            @api.route('/echo', methods=['get', 'post'])
+            @api.args({'name': str})
+            def greet(name="world"):
+                return 'Hello ' + name
+        """
+        def _make_decorator(argmap, targets):
+            parser = ArgParser(argmap, targets)
+            def decorator(func):
+                @wraps(func)
+                def decorated(*args, **kwargs):
+                    parsed_args = parser.parse()
+                    parsed_args.update(kwargs)
+                    return func(*args, **parsed_args)
+                return decorated
+            return decorator
+        return _make_decorator(argmap, targets)
+
     def _json_dumps(self, obj, **kwargs):
         """Dumps object to json string. """
         kwargs.setdefault('ensure_ascii', False)
@@ -50,3 +73,61 @@ class API(Blueprint):
         """Get a json response. """
         return Response(self._json_dumps(obj),
                         mimetype='application/json')
+
+
+class ArgParser(object):
+    """Flask request argument parser."""
+
+    DEFAULT_TARGETS = ('querystring', 'json', 'form')
+
+    def __init__(self, argmap, targets):
+        self.argmap = argmap
+        self.missing = object()
+        self.targets = targets or self.DEFAULT_TARGETS
+
+    def get_value(self, data, name):
+        return data.get(name, self.missing)
+
+    def parse_value(self, name):
+        """Pull a form value from the request."""
+        for target in self.targets:
+            if target == "querystring":
+                value = self.get_value(request.args, name)
+            elif target == "json":
+                json_data = request.get_json(silent=True)
+                if json_data:
+                    value = self.get_value(json_data, name)
+                else:
+                    value = self.missing
+            elif target == "form":
+                value = self.get_value(request.form, name)
+            if value is not self.missing:
+                return value
+        return self.missing
+
+    def convert(self, value, argtype):
+        if argtype == str:
+            return to_unicode(value)
+        else:
+            return argtype(value)
+
+    def parse(self):
+        """Parses the request arguments."""
+        kwargs = {}
+        for argname, argtype in iteritems(self.argmap):
+            value = self.parse_value(argname)
+            if value is not self.missing:
+                try:
+                    kwargs[argname] = self.convert(value, argtype)
+                except:
+                    try:
+                        abort(400)
+                    except:
+                        exc_type, exc_value, tb = sys.exc_info()
+                        exc_value.data = \
+                            ("The parameter '%s' specified in the request "
+                            "URI is not supported." % argname)
+                        reraise(exc_type, exc_value, tb.tb_next)
+        return kwargs
+
+
