5d35f0092090c10b422ab7a39afb1e5f29df97d1
Salem Harrache <salem.harrache@inria.fr>
2015-02-17 17:19:44 +0100

    improved performance of the copy operation

--- oar_utils/db/operations.py
+++ oar_utils/db/operations.py
@@ -5,11 +5,13 @@ from copy import copy
 
 from sqlalchemy import func
 from sqlalchemy.engine.reflection import Inspector
+from sqlalchemy.types import Integer
 from sqlalchemy.sql.expression import select
 from sqlalchemy_utils.functions import database_exists, create_database
+from sqlalchemy.ext.declarative import declarative_base
 
 from oar.lib import db, Database
-from oar.lib.compat import iteritems, itervalues, to_unicode
+from oar.lib.compat import  itervalues, to_unicode
 
 
 from .helpers import log, green, magenta, yellow, blue, red
@@ -28,87 +30,96 @@ def local_database(engine_url):
 def sync_db(to_db_url, chunk_size=1000):
     to_db = Database(uri=to_db_url)
     engine_url = to_db.engine.url
-    dialect = db.engine.dialect.name
     if not database_exists(engine_url) and local_database(engine_url):
-        if dialect in ("postgresql", "mysql"):
+        if db.engine.dialect.name in ("postgresql", "mysql"):
             copy_db(to_db)
-    create_all_tables(to_db)
-    copy_tables(to_db, chunk_size)
-    update_sequences(to_db)
+
+    tables = sync_schema(to_db)
+    sync_tables(tables, to_db, chunk_size)
+    fix_sequences(to_db)
 
 
-def create_all_tables(to_db):
+def sync_schema(to_db):
     db.reflect()
-    db.create_all()
-    inspector = Inspector.from_engine(to_db.engine)
-    existing_tables = inspector.get_table_names()
+    existing_tables = Inspector.from_engine(to_db.engine).get_table_names()
     for table in db.metadata.sorted_tables:
         if table.name not in existing_tables:
             log(' %s ~> table %s' % (green('create'), table.name))
             try:
                 table.create(bind=to_db.engine, checkfirst=True)
+                yield table
             except Exception as ex:
                 log(*red(to_unicode(ex)).splitlines(), prefix=(' ' * 9))
+        else:
+            # Make sure we have the good version of the table
+            yield db.Table(table.name, db.MetaData(db.engine), autoload=True)
 
 
-def copy_tables(to_db, chunk_size):
-    for name, Model in iteritems(db.models):
-        copy_model(Model, to_db, chunk_size)
-    # Tables without primary keys
-    tables_with_pk = (model.__table__ for model in itervalues(db.models))
-    all_tables = (table for table in itervalues(db.tables))
-    for table in set(all_tables) - set(tables_with_pk):
-        empty_table(table, to_db)
-        copy_table(table, to_db, chunk_size)
-
-
-def merge_model(Model, to_db):
-    log(' %s ~> table %s' % (magenta(' merge'), Model.__table__.name))
-    query_result = db.query(Model)
-    for r in query_result:
-         to_db.session.merge(r)
-    to_db.session.commit()
-
-
-def copy_model(Model, to_db, chunk_size):
+def sync_tables(tables, to_db, chunk_size):
     # prepare the connection
-    to_connection = to_db.engine.connect()
+    raw_conn = to_db.engine.connect()
     # Get the max pk
-    pk = Model.__mapper__.primary_key[0]
-    max_pk_query = select([func.max(pk)])
-    max_pk = to_connection.execute(max_pk_query).scalar()
-    condition = None
-    if max_pk is not None:
-        condition = (pk > max_pk)
-    table = Model.__table__
-    copy_table(table, to_db, chunk_size, select_condition=condition)
+    for table in tables:
+        if table.primary_key:
+            pk = table.primary_key.columns.values()[0]
+            if isinstance(pk.type, Integer):
+                max_pk_query = select([func.max(pk)])
+                max_pk = raw_conn.execute(max_pk_query).scalar()
+                cond = None
+                if max_pk is not None:
+                    cond = (pk > max_pk)
+                copy_table(table, raw_conn, chunk_size, condition=cond)
+            else:
+                merge_table(table, to_db.session)
+        else:
+            log(magenta('  empty') + ' ~> table ' + table.name)
+            delete_from_tables(table, raw_conn)
+            copy_table(table, raw_conn, chunk_size)
 
 
-def copy_table(table, to_db, chunk_size, select_condition=None):
+def merge_table(table, session):
+    ## Very slow !!
+    log(' %s ~> table %s' % (magenta(' merge'), table.name))
+    Model = generic_mapper(table)
+    columns = table.columns.keys()
+    for record in db.query(table).all():
+        data = dict(
+            [(str(column), getattr(record, column)) for column in columns]
+        )
+        session.merge(Model(**data))
+    session.commit()
+
+
+def delete_from_tables(table, raw_conn, condition=None):
+    delete_query = table.delete()
+    if condition is not None:
+        delete_query = delete_query.where(condition)
+    raw_conn.execute(delete_query)
+
+
+def copy_table(table, raw_conn, chunk_size, condition=None):
     # prepare the connection
-    to_connection = to_db.engine.connect()
-    from_connection = db.engine.connect()
+    from_conn = db.engine.connect()
 
     insert_query = table.insert()
     select_table = select([table])
     select_count = select([func.count()]).select_from(table)
-    if select_condition is not None:
-        select_query = select_table.where(select_condition)
-        count_query = select_count.where(select_condition)
+    if condition is not None:
+        select_query = select_table.where(condition)
+        count_query = select_count.where(condition)
     else:
         select_query = select_table
         count_query = select_count
 
-    total_lenght = from_connection.execute(count_query).scalar()
-    result = from_connection.execution_options(stream_results=True)\
+    total_lenght = from_conn.execute(count_query).scalar()
+    result = from_conn.execution_options(stream_results=True)\
                             .execute(select_query)
-
     message = yellow('\r   copy') + ' ~> table %s (%s)'
     log(message % (table.name, blue("0/%s" % total_lenght)), nl=False)
     if total_lenght > 0:
         progress = 0
         while True:
-            transaction = to_connection.begin()
+            transaction = raw_conn.begin()
             rows = result.fetchmany(chunk_size)
             lenght = len(rows)
             if lenght == 0:
@@ -116,18 +127,13 @@ def copy_table(table, to_db, chunk_size, select_condition=None):
             progress = lenght + progress
             percentage = blue("%s/%s" % (progress, total_lenght))
             log(message % (table.name, percentage), nl=False)
-            to_connection.execute(insert_query, rows)
+            raw_conn.execute(insert_query, rows)
             del rows
-            transaction.commit()
+        transaction.commit()
     log("")
 
 
-def empty_table(table, to_db):
-    log(magenta('  empty') + ' ~> table ' + table.name)
-    to_db.engine.execute(table.delete())
-
-
-def update_sequences(to_db):
+def fix_sequences(to_db):
     engine = to_db.engine
     def get_sequences_values():
         for model in itervalues(db.models):
@@ -196,3 +202,10 @@ def copy_db(to_db):
             ))
     else:
         raise NotSupportedDatabase()
+
+
+def generic_mapper(table):
+    Base = declarative_base()
+    class GenericMapper(Base):
+        __table__ = table
+    return GenericMapper
