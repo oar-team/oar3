d9896e1cbf240aadb12c8b6679065dcc894af3ca
Salem Harrache <salem.harrache@inria.fr>
2015-02-25 15:10:49 +0100

    Moved --ignore filters options to subcommands (sync/purge)

--- oar_utils/db/archive.py
+++ oar_utils/db/archive.py
@@ -17,39 +17,43 @@ CONTEXT_SETTINGS = dict(auto_envvar_prefix='oar',
 @click.option('-y', '--force-yes', is_flag=True, default=False,
               help="Never prompts for user intervention")
 @click.option('--db-suffix', default="archive", help="Archive database suffix")
-@click.option('--ignore-resources', default=["^Dead"], multiple=True)
-@click.option('--ignore-jobs', default=["^Terminated", "^Error"],
-              multiple=True)
-@click.option('--jobs-older-than', default="1Y")
 @click.option('--debug', is_flag=True, default=False, help="Enable Debug.")
 @pass_context
-def cli(ctx, force_yes, db_suffix, debug, ignore_resources, ignore_jobs, 
-        jobs_older_than):
+def cli(ctx, force_yes, db_suffix, debug):
     """Archive OAR database."""
     ctx.force_yes = force_yes
     ctx.archive_db_suffix = db_suffix
     ctx.debug = debug
-    ctx.ignore_resources = ignore_resources
-    ctx.ignore_jobs = ignore_jobs
     ctx.print_db_info()
     config["LOG_FORMAT"] = '[%(levelname)s]: %(message)s'
 
 
 @cli.command()
 @click.option('--chunk', type=int, default=10000, help="Chunk size")
+@click.option('--ignore-jobs', default=["^Terminated", "^Error"],
+              multiple=True)
 @pass_context
-def sync(ctx, chunk):
-    """ Send old resources and jobs to archive database."""
+def sync(ctx, chunk, ignore_jobs):
+    """ Send all resources and finished jobs to archive database."""
     ctx.chunk = chunk
+    ctx.ignore_jobs = ignore_jobs
     ctx.confirm("Continue to copy old resources and jobs to the archive "
                 "database?")
     copy_db(ctx)
 
 
 @cli.command('purge')
+@click.option('--ignore-jobs', default=["^Terminated", "^Error"],
+              multiple=True)
+@click.option('--jobs-older-than', default="1Y")
+@click.option('--ignore-resources', default=["^Dead"], multiple=True)
 @pass_context
-def purge(ctx):
+def purge(ctx, ignore_resources, ignore_jobs, jobs_older_than):
     """ Purge old resources and old jobs from your current database."""
+    ctx._cache = {}  ## reset filters in case of chained commands
+    ctx.ignore_resources = ignore_resources
+    ctx.ignore_jobs = ignore_jobs
+    ctx.jobs_older_than = jobs_older_than
     msg = "Continue to purge old resources and jobs "\
           "from your current database?"
     ctx.confirm(click.style(msg.upper(), underline=True, bold=True))
