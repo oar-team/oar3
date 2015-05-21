96ce89d6b49f39e717f0060c096035a9bb46fbe3
Salem Harrache <salem.harrache@inria.fr>
2015-05-21 15:37:04 +0200

    Used more explicit arguments name

--- oar_rest_api/views/job.py
+++ oar_rest_api/views/job.py
@@ -15,19 +15,22 @@ app = Blueprint('jobs', __name__, url_prefix='/jobs')
 
 
 @app.route('/', methods=['GET'])
-@app.route('/<any(details, table):details>', methods=['GET'])
+@app.route('/<any(details, table):detailed>', methods=['GET'])
 @app.route('/nodes/<string:network_address>', methods=['GET'])
 @app.route('/ressources/<string:resource_id>/details', methods=['GET'])
 @app.args({'offset': Arg(int, default=0),
            'limit': Arg(int),
            'user': Arg(str),
-           'from': Arg(int, dest='from_'),
-           'to': Arg(int),
+           'from': Arg(int, dest='start_time'),
+           'to': Arg(int, dest='stop_time'),
            'state': Arg([str, ','], dest='states'),
            'array': Arg(int, dest='array_id'),
-           'ids': Arg([int, ':'])})
+           'ids': Arg([int, ':'], dest='job_ids')})
 @app.need_authentication()
-def index(offset, limit, user, from_, to, states, array_id, ids, details=None):
+def index(offset, limit, user, start_time, stop_time, states, array_id,
+          job_ids, detailed=False):
+    query = db.queries.get_jobs_for_user(user, start_time, stop_time,
+                                         states, array_id, job_ids, detailed)
     g.data['total'] = page.total
     g.data['links'] = [{'rel': 'self', 'href': page.url}]
     if page.has_next:
--- oar_rest_api/views/resource.py
+++ oar_rest_api/views/resource.py
@@ -12,36 +12,38 @@ from ..utils import Arg
 app = Blueprint('resources', __name__, url_prefix="/resources")
 
 
-def get_links(resource_id, network_address):
+def get_items_links(resource_id, network_address):
     rel_map = (
         ("node", "member", "index"),
         ("show", "self", "show"),
         ("jobs", "collection", "jobs"),
     )
+    links = []
     for title, rel, endpoint in rel_map:
         if title == "node":
             url = url_for('.%s' % endpoint, network_address=network_address)
         else:
             url = url_for('.%s' % endpoint, resource_id=resource_id)
-        yield {'rel': rel, 'href': url, 'title': title}
+        links.append({'rel': rel, 'href': url, 'title': title})
+    return links
 
 
 @app.route('/', methods=['GET'])
-@app.route('/<any(details, full):details>', methods=['GET'])
+@app.route('/<any(details, full):detailed>', methods=['GET'])
 @app.route('/details', methods=['GET'])
 @app.route('/nodes/<string:network_address>', methods=['GET'])
 @app.route('/nodes/<string:network_address>/details', methods=['GET'])
 @app.args({'offset': Arg(int, default=0),
            'limit': Arg(int)})
-def index(offset, limit, network_address=None, details=None):
-    query = db.queries.get_resources(network_address, details)
+def index(offset, limit, network_address=None, detailed=False):
+    query = db.queries.get_resources(network_address, detailed)
     page = query.paginate(offset, limit)
     g.data['total'] = page.total
     g.data['links'] = page.links
     g.data['offset'] = offset
     g.data['items'] = []
     for item in page:
-        item['links'] = list(get_links(item["id"], item["network_address"]))
+        item['links'] = get_items_links(item["id"], item["network_address"])
         g.data['items'].append(item)
 
 
@@ -49,7 +51,7 @@ def index(offset, limit, network_address=None, details=None):
 def show(resource_id):
     resource = Resource.query.get_or_404(resource_id)
     g.data.update(resource.asdict())
-    g.data['links'] = list(get_links(resource.id, resource.network_address))
+    g.data['links'] = get_items_links(resource.id, resource.network_address)
 
 
 @app.route('/<int:resource_id>/jobs', methods=['GET'])
