f397763e55d4dacec3c58ceed4d1948e9e51a5dd
Salem Harrache <salem.harrache@inria.fr>
2015-06-12 15:31:59 +0200

    Fixed pg_bulk_insert encoding errors

--- oar_utils/db/psycopg2.py
+++ oar_utils/db/psycopg2.py
@@ -2,26 +2,31 @@
 from __future__ import division, absolute_import, unicode_literals
 
 import random
+
+from io import open
 from multiprocessing import Process
+
 from oar.lib.utils import make_temp_fifo
 
 
-def serialize_result_proxy_to_csv(rows, null_value, output):
+def serialize_result_proxy_to_csv(rows, null_value, output_file):
     def escape_row(row):
-        for value in row.values():
+        for value in row:
             if value is None:
-                value = null_value
+                yield null_value
             elif isinstance(value, basestring):
-                if '\n' in value or '\r' in value or '\t' in value:
-                    value = value.replace('\r', '\\r')\
-                                 .replace('\n', '\\n')\
-                                 .replace('\t', '\\t')
+                if '\n' in value or '\t' in value or '\r' in value:
+                    yield value.replace('\n', '\\n')\
+                               .replace('\t', '\\t')\
+                               .replace('\r', '\\r')
+                else:
+                    yield value
             else:
-                value = "%s" % value
-            yield value
+                yield value.__str__()
 
-    for row in rows:
-        output.write("\t".join(escape_row(row)) + "\n")
+    with open(output_file, 'w', encoding="utf-8") as output:
+        for row in rows:
+            output.write("\t".join(escape_row(row)) + "\n")
 
 
 def pg_bulk_insert(cursor, table, rows, columns):
@@ -30,8 +35,7 @@ def pg_bulk_insert(cursor, table, rows, columns):
         null_value = 'None_%x' % random.getrandbits(32)
 
         def writer_process():
-            with open(fifo, 'w') as fifo_output:
-                serialize_result_proxy_to_csv(rows, null_value, fifo_output)
+            serialize_result_proxy_to_csv(rows, null_value, fifo)
 
         writer = Process(target=writer_process)
         writer.start()
