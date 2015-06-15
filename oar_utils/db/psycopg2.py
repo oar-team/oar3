d5cff9b1a84dd32369a68150a9b2eaf526eb3d3d
Salem Harrache <salem.harrache@inria.fr>
2015-06-15 21:56:50 +0200

    ignored Vagrantfile

--- oar_utils/db/psycopg2.py
+++ oar_utils/db/psycopg2.py
@@ -7,6 +7,7 @@ from io import open
 from multiprocessing import Process
 
 from oar.lib.utils import make_temp_fifo
+from werkzeug.contrib.iterio import IterIO
 
 
 def serialize_result_proxy_to_csv(rows, null_value, output_file):
@@ -24,25 +25,15 @@ def serialize_result_proxy_to_csv(rows, null_value, output_file):
             else:
                 yield value.__str__()
 
-    with open(output_file, 'w', encoding="utf-8") as output:
-        for row in rows:
-            output.write("\t".join(escape_row(row)) + "\n")
+    for row in rows:
+        yield "\t".join(escape_row(row)) + "\n"
 
 
 def pg_bulk_insert(cursor, table, rows, columns):
-    with make_temp_fifo(table.name) as fifo:
+    null_value = 'None_%x' % random.getrandbits(32)
 
-        null_value = 'None_%x' % random.getrandbits(32)
-
-        def writer_process():
-            serialize_result_proxy_to_csv(rows, null_value, fifo)
-
-        writer = Process(target=writer_process)
-        writer.start()
-
-        with open(fifo, 'r') as fifo_input:
-            cursor.copy_from(fifo_input,
-                             "%s" % table.name,
-                             columns=columns,
-                             null=null_value)
-        writer.join()
+    iter_stream = IterIO(serialize_result_proxy_to_csv(rows, null_value))
+    cursor.copy_from(iter_stream,
+                     "%s" % table.name,
+                     columns=columns,
+                     null=null_value)
