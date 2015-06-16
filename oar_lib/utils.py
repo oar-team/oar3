cecf035b860c26e0bdae49576171c7a15761090b
Salem Harrache <salem.harrache@inria.fr>
2015-06-16 21:09:49 +0200

    Added IterStream class that give a stream like interface for any iterator

--- oar_lib/utils.py
+++ oar_lib/utils.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import with_statement, absolute_import, unicode_literals
 
-import os
-import tempfile
-
-from contextlib import contextmanager
-
 from .compat import numeric_types
 
 
@@ -50,13 +45,65 @@ class CachedProperty(object):
 cached_property = CachedProperty
 
 
-@contextmanager
-def make_temp_fifo(name, prefix='tmp'):
-    tmpdir = tempfile.mkdtemp(prefix=prefix)
-    filename = os.path.join(tmpdir, '%s.fifo' % name)
-    os.mkfifo(filename)
-    yield filename
-    if os.path.exists(filename):
-        os.remove(filename)
-    if os.path.isdir(tmpdir):
-        os.rmdir(tmpdir)
+class IterStream(object):
+    """Wrap an iterator and give it a stream like interface.
+    Snippet from werkzeug project.
+    """
+
+    def __new__(cls, gen, sentinel=''):
+        self = object.__new__(cls)
+        self._gen = gen
+        self._buf = None
+        self.sentinel = sentinel
+        self.pos = 0
+        return self
+
+    def __iter__(self):
+        return self
+
+    def _mixed_join(self, iterable):
+        """concatenate any string type in an intelligent way."""
+        iterator = iter(iterable)
+        first_item = next(iterator, self.sentinel)
+        if isinstance(first_item, bytes):
+            return first_item + b''.join(iterator)
+        return first_item + u''.join(iterator)
+
+    def _buf_append(self, string):
+        '''Replace string directly without appending to an empty string,
+        avoiding type issues.'''
+        if not self._buf:
+            self._buf = string
+        else:
+            self._buf += string
+
+    def read(self, n=-1):
+        if n < 0:
+            self._buf_append(self._mixed_join(self._gen))
+            result = self._buf[self.pos:]
+            self.pos += len(result)
+            return result
+        new_pos = self.pos + n
+        buf = []
+        try:
+            tmp_end_pos = 0 if self._buf is None else len(self._buf)
+            while new_pos > tmp_end_pos or (self._buf is None and not buf):
+                item = next(self._gen)
+                tmp_end_pos += len(item)
+                buf.append(item)
+        except StopIteration:
+            pass
+        if buf:
+            self._buf_append(self._mixed_join(buf))
+
+        if self._buf is None:
+            return self.sentinel
+
+        new_pos = max(0, new_pos)
+        try:
+            return self._buf[self.pos:new_pos]
+        finally:
+            self.pos = min(new_pos, len(self._buf))
+
+    def readline(self):
+        raise NotImplementedError()
