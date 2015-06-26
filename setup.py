17157095f58a5eab5736c2117341e824598abd1f
Salem Harrache <salem.harrache@inria.fr>
2015-06-26 14:32:07 +0200

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,47 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import re
 import os.path as op
-from setuptools import setup
 
-here = op.abspath(op.dirname(__file__))
+
+from codecs import open
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def read(fname):
+    ''' Return the file content. '''
+    here = op.abspath(op.dirname(__file__))
+    with open(op.join(here, fname), 'r', 'utf-8') as fd:
+        return fd.read()
+
+readme = read('README.rst')
+history = read('HISTORY.rst').replace('.. :changelog:', '')
 
 requirements = [
     'oar-lib',
     'flask>=0.10',
 ]
 
+version = ''
+version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                    read(op.join('oar_rest_api', '__init__.py')),
+                    re.MULTILINE).group(1)
 
-def read(fname):
-    ''' Return the file content. '''
-    with open(op.join(here, fname)) as fd:
-        return fd.read()
-
-
-def get_version():
-    return re.compile(r".*__version__ = '(.*?)'", re.S)\
-             .match(read(op.join('oar_rest_api', '__init__.py'))).group(1)
+if not version:
+    raise RuntimeError('Cannot find version information')
 
 
 setup(
     name='oar-rest-api',
     author='Salem Harrache',
     author_email='salem.harrache@inria.fr',
-    version=get_version(),
+    version=version,
     url='https://github.com/oar-team/python-oar-rest-api',
     packages=['oar_rest_api'],
     install_requires=requirements,
     include_package_data=True,
     zip_safe=False,
     description='Python OAR RESTful API',
-    long_description=read('README.rst') + '\n\n' + read('CHANGES'),
+    long_description=readme + '\n\n' + history,
     license="GNU GPL",
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Operating System :: OS Independent',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: BSD License',
         'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Clustering',
     ],
