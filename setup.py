2d341d0e621bc318917fc0b7ae42291ebeaedc4e
Salem Harrache <salem.harrache@inria.fr>
2015-06-30 12:30:59 +0200

    Update setup.py and renamed pypi package

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
-import os.path as op
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import re
-from setuptools import setup
+import os.path as op
+
+
+from codecs import open
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    ''' Return the file content. '''
+    here = op.abspath(op.dirname(__file__))
+    with open(op.join(here, fname), 'r', 'utf-8') as fd:
+        return fd.read()
+
+readme = read('README.rst')
+changelog = read('CHANGES.rst').replace('.. :changelog:', '')
 
 requirements = [
     'oar-lib',
@@ -8,35 +23,35 @@ requirements = [
     'SimPy',
 ]
 
-here = op.abspath(op.dirname(__file__))
+version = ''
+version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                    read(op.join('oar_kao', '__init__.py')),
+                    re.MULTILINE).group(1)
 
-def read(fname):
-    ''' Return the file content. '''
-    with open(op.join(here, fname)) as fd:
-        return fd.read()
-
-
-def get_version():
-    return re.compile(r".*__version__ = '(.*?)'", re.S)\
-             .match(read(op.join(here, 'oar_kao', '__init__.py'))).group(1)
+if not version:
+    raise RuntimeError('Cannot find version information')
 
 
 setup(
-    name='kao',
+    name='oar-kao',
     author='Olivier Richard',
     author_email='olivier.richard@imag.fr',
-    version=get_version(),
+    version=version,
     url='https://github.com/oar-team/kao',
     install_requires=requirements,
-    packages=['oar_kao'],
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     description='Another Metascheduler for OAR.',
-    long_description=read('README.rst') + '\n\n' + read('CHANGES'),
+    long_description=readme + '\n\n' + changelog,
+    keywords='oar-kao',
+    license='BSD',
     classifiers=[
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Topic :: System :: Clustering',
     ],
     entry_points='''
         [console_scripts]
