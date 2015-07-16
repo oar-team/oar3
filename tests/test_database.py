6691a5818a6867ae9e4da0aa4ef14378143a15d3
Salem Harrache <salem.harrache@inria.fr>
2015-07-16 11:43:48 +0200

    Used db pytest fixture for database testing

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import pytest
 import datetime
 
 from sqlalchemy import event
@@ -8,6 +9,7 @@ from oar.lib.compat import StringIO, to_unicode
 
 
 class EngineListener(object):
+
     def __init__(self, engine, ignored=('PRAGMA')):
         self.engine = engine
         self.ignored = ignored
@@ -30,9 +32,11 @@ class EngineListener(object):
                                   .rstrip('\n')
 
 
-def declare_schema(db):
+@pytest.fixture
+def db(request, monkeypatch):
+    db = Database(uri='sqlite://')
 
-    class Actor(db.Model, db.DeferredReflection):
+    class Actor(db.DeferredReflectionModel):
         __table_args__ = (db.Index('name', 'lastname', 'firstname'), )
 
         id = db.Column(db.Integer, primary_key=True)
@@ -41,69 +45,60 @@ def declare_schema(db):
         birthday = db.Column(db.DateTime, nullable=False,
                              default=datetime.datetime.utcnow)
 
-    category = db.Table('categories',
-                        db.Column('name', db.String(255)),
-                        db.Column('description', db.String(255)))
+    db.Table('categories',
+             db.Column('name', db.String(255)),
+             db.Column('description', db.String(255))
+             )
 
-    return Actor, category
+    return db
 
 
-def test_sqlite_schema():
-    db = Database(uri='sqlite://')
+def test_sqlite_schema(db):
     engine_listener = EngineListener(db.engine)
-    declare_schema(db)
     db.create_all()
 
-    expected_schema = """
+    expected_schemas = ("""
 CREATE TABLE categories (
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL
-);
-
+);""", """
 CREATE TABLE actor (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     firstname VARCHAR(20) NOT NULL,
     lastname VARCHAR(20) NOT NULL,
     birthday DATETIME NOT NULL
-);
+);""", """
 CREATE INDEX name ON actor (lastname, firstname);"""
-    assert engine_listener.raw_sql == expected_schema
+                        )
+    for schema in expected_schemas:
+        assert schema in engine_listener.raw_sql
 
 
-def test_model_args():
-    db = Database(uri='sqlite://')
-    Actor, _ = declare_schema(db)
+def test_model_args(db):
     db.create_all()
-    assert Actor.__table__.name == "actor"
-    index_columns = list(list(Actor.__table__.indexes)[0].columns)
+    assert db['actor'].name == "actor"
+    index_columns = list(list(db['actor'].indexes)[0].columns)
     assert index_columns[0].name == "lastname"
     assert index_columns[1].name == "firstname"
 
 
-def test_deferred_reflection():
-    db = Database(uri='sqlite://')
-    Actor, _ = declare_schema(db)
+def test_collected_tables_and_models(db):
+    db.create_all()
+    # assert db.tables == {'actor': Actor.__table__, 'categories': category}
+    # assert db.models == {'Actor': Actor}
+    assert "Actor" in db
+    assert "actor" in db
+    assert "categories" in db
+
+
+def test_deferred_reflection(db):
     db.create_all()
     db.op.add_column('actor', db.Column('salary', db.Integer,
                                         nullable=True,
                                         default=1000000))
     db.reflect()
-    Actor.create(firstname="Ben", lastname="Affleck", salary=12000000)
-    ben = Actor.query.first()
-    keys = ben.asdict().keys()
+    db['Actor'].create(firstname="Ben", lastname="Affleck", salary=12000000)
+    ben = db['Actor'].query.first()
+    keys = list(ben.asdict().keys())
     assert ben.salary == 12000000
     assert ['id', 'firstname', 'lastname', 'birthday', 'salary'] == keys
-
-
-def test_collected_tables_and_models():
-    db = Database(uri='sqlite://')
-    Actor, category = declare_schema(db)
-    db.create_all()
-    db.op.add_column('actor', db.Column('salary', db.Integer,
-                                        nullable=True,
-                                        default=1000000))
-    assert db.tables == {'actor': Actor.__table__, 'categories': category}
-    assert db.models == {'Actor': Actor}
-    assert "Actor" in db
-    assert "actor" in db
-    assert "categories" in db
