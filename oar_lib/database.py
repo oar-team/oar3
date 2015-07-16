e35ca9a55ba43d14e881f62a607910b27e00ef67
Salem Harrache <salem.harrache@inria.fr>
2015-07-16 14:25:27 +0200

    Fix db.close method by removing deadlock

--- oar_lib/database.py
+++ oar_lib/database.py
@@ -226,9 +226,9 @@ class Database(object):
 
     def close(self, **kwargs):
         """Proxy for Session.close"""
+        self.session.close()
         with self._engine_lock:
             if self.connector is not None:
-                self.session.close()
                 self.connector.get_engine().dispose()
                 self.connector = None
 
