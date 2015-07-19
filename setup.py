3211d2a1de2541884dded9580c314943e7a998c9
Salem Harrache <salem.harrache@inria.fr>
2015-07-19 15:32:11 +0200

    updated setuptools install script

--- setup.py
+++ setup.py
@@ -1,43 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import re
 import os.path as op
-from setuptools import setup
 
-here = op.abspath(op.dirname(__file__))
+
+from codecs import open
+from setuptools import setup
 
 
 def read(fname):
     ''' Return the file content. '''
-    with open(op.join(here, fname)) as fd:
+    here = op.abspath(op.dirname(__file__))
+    with open(op.join(here, fname), 'r', 'utf-8') as fd:
         return fd.read()
 
+readme = read('README.rst')
+history = read('CHANGES.rst').replace('.. :changelog:', '')
 
-def get_version():
-    return re.compile(r".*__version__ = '(.*?)'", re.S)\
-             .match(read(op.join(here, 'oar_lib', '__init__.py'))).group(1)
+requirements = [
+    'sqlalchemy',
+    'alembic',
+]
+
+version = ''
+version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                    read(op.join('oar_lib', '__init__.py')),
+                    re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
 
 setup(
     name='oar-lib',
     author='Salem Harrache',
     author_email='salem.harrache@inria.fr',
-    version=get_version(),
+    version=version,
+    description='Python version of OAR Common Library',
+    long_description=readme + '\n\n' + history,
+    keywords='oar-lib',
     url='https://github.com/oar-team/python-oar-lib',
     packages=['oar', 'oar_lib'],
-    install_requires=[
-        'sqlalchemy',
-        'sqlalchemy-utils',
-        'alembic',
-    ],
+    install_requires=requirements,
     include_package_data=True,
     zip_safe=False,
-    description='OAR common lib.',
     license="GNU GPL v2",
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Operating System :: OS Independent',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'Programming Language :: Python',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',  # noqa
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Clustering',
     ]
