e88b31c36f6dc1d0635f3a5c1e0f20ecd3655944
Salem Harrache <salem.harrache@inria.fr>
2015-07-19 10:03:19 +0200

    Tested row2dict helper function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,11 +2,32 @@
 import time
 import random
 
+import pytest
+
 from collections import OrderedDict
 
 from oar.lib import Database
 from oar.lib.utils import (SimpleNamespace, cached_property, ResultProxyIter,
-                           try_convert_decimal)
+                           try_convert_decimal, row2dict)
+
+
+@pytest.fixture
+def db(request, monkeypatch):
+    db = Database(uri='sqlite://')
+
+    table = db.Table(
+        'table',
+        db.Column('num', db.Integer),
+        db.Column('word', db.String(255))
+    )
+    db.create_all()
+    db.session.execute(table.insert(), ({'num': 1, 'word': 'one'},
+                                        {'num': 2, 'word': 'two'},
+                                        {'num': 3, 'word': 'three'},
+                                        {'num': 4, 'word': 'four'}))
+
+    db.commit()
+    return db
 
 
 def test_simple_namespace():
@@ -34,22 +55,17 @@ def test_cached_propery():
     assert myobject.value != value
 
 
-def test_result_proxy_iter():
-    db = Database(uri='sqlite://')
+def test_row2dict(db):
+    item = db.session.execute(db['table'].select().limit(1)).fetchall()[0]
+    assert set(item.keys()) == {'num', 'word'}
+    assert set(row2dict(item).keys()) == {'num', 'word'}
+    assert isinstance(row2dict(item), OrderedDict)
+    assert set(row2dict(item, ignore_keys=('num',)).keys()) == {'word'}
+    assert row2dict(item, ignore_keys=('num', 'word')) == OrderedDict()
 
-    table = db.Table(
-        'table',
-        db.Column('num', db.Integer),
-        db.Column('word', db.String(255))
-    )
-    db.create_all()
-    db.session.execute(table.insert(), ({'num': 1, 'word': 'one'},
-                                        {'num': 2, 'word': 'two'},
-                                        {'num': 3, 'word': 'three'},
-                                        {'num': 4, 'word': 'four'}))
 
-    db.commit()
-    result_proxy = ResultProxyIter(db.session.execute(table.select()))
+def test_result_proxy_iter(db):
+    result_proxy = ResultProxyIter(db.session.execute(db['table'].select()))
     assert len(result_proxy) == 4
 
     # result_proxy is an iterator
