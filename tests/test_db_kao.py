e71aaf2fbe9b68dfabcb15c396d83209b07c8356
Olivier Richard <olivier.richard@imag.fr>
2015-07-21 14:27:16 +0200

    Add tests for kao

---
+++ tests/test_db_kao.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+from __future__ import unicode_literals, print_function
+import pytest
+
+from oar.lib import db
+from oar.kao.job import insert_job
+from oar.kao.kao import main
+
+import oar.kao.utils  # for monkeypatching
+
+
+@pytest.fixture(scope="module", autouse=True)
+def create_db(request):
+    db.create_all()
+    db.reflect()
+    db.delete_all()
+
+    @request.addfinalizer
+    def teardown():
+        db.delete_all()
+        db.session.close()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def minimal_db_initialization(request):
+    db['Queue'].create(name='default', priority=3, scheduler_policy='kamelot', state='Active')
+
+    # add some resources
+    for i in range(5):
+        db['Resource'].create(network_address="localhost")
+
+    @request.addfinalizer
+    def teardown():
+        db.delete_all()
+        db.session.close()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def monkeypatch_utils(request, monkeypatch):
+    monkeypatch.setattr(oar.kao.utils, 'init_judas_notify_user', lambda: None)
+    monkeypatch.setattr(oar.kao.utils, 'create_almighty_socket', lambda: None)
+    monkeypatch.setattr(oar.kao.utils, 'notify_almighty', lambda x: len(x))
+    monkeypatch.setattr(oar.kao.utils, 'notify_tcp_socket', lambda addr, port, msg: len(msg))
+    monkeypatch.setattr(oar.kao.utils, 'notify_user', lambda job, state, msg: len(state + msg))
+
+
+def test_db_koa_simple_1(monkeypatch):
+    insert_job(res=[(60, [('resource_id=4', "")])], properties="")
+    job = db['Job'].query.one()
+    print('job state:', job.state)
+
+    # pdb.set_trace()
+    main()
+
+    for i in db['GanttJobsPrediction'].query.all():
+        print("moldable_id: ", i.moldable_id, ' start_time: ', i.start_time)
+
+    job = db['Job'].query.one()
+    print(job.state)
+
+    assert (job.state == 'toLaunch')
