d1927a394c328a9cb66b0c15e73993928b17014b
Salem Harrache <salem.harrache@inria.fr>
2015-09-23 15:40:51 +0200

    Tests on postgresql and sqlite-file/memory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,51 @@
+# coding: utf-8
+from __future__ import unicode_literals, print_function
+
+import os
+import tempfile
+import shutil
 import pytest
 
+from oar.lib import config, db
 from . import DEFAULT_CONFIG
 
 
-@pytest.fixture(scope="module", autouse=True)
-def config(request):
-    # Create the tables based on the current model
-    from oar.lib import config
+@pytest.yield_fixture(scope='session', autouse=True)
+def setup_config(request):
     config.clear()
     config.update(DEFAULT_CONFIG.copy())
-    return config
+    tempdir = tempfile.mkdtemp()
+    config["LOG_FILE"] = os.path.join(tempdir, 'oar.log')
+
+    db_type = os.environ.get('DB_TYPE', 'memory')
+
+    if db_type not in ('memory', 'sqlite', 'postgresql'):
+        pytest.exit("Unsupported database '%s'" % db_type)
+
+    if db_type == "sqlite":
+        config['DB_BASE_FILE'] = os.path.join(tempdir, 'db.sqlite')
+        config['DB_TYPE'] = 'sqlite'
+    elif db_type == "memory":
+        config['DB_TYPE'] = 'sqlite'
+        config['DB_BASE_FILE'] = ':memory:'
+    else:
+        config['DB_TYPE'] = 'Pg'
+        config['DB_PORT'] = '5432'
+        config['DB_BASE_NAME'] = 'oar'
+        config['DB_BASE_PASSWD'] = 'oar'
+        config['DB_BASE_LOGIN'] = 'oar'
+        config['DB_BASE_PASSWD_RO'] = 'oar'
+        config['DB_BASE_LOGIN_RO'] = 'oar'
+        config['DB_HOSTNAME'] = 'localhost'
+
+    db.create_all()
+    with db.session(ephemeral=True, reflect=False):
+        kw = {"nullable": True}
+        db.op.add_column('resources', db.Column('core', db.Integer, **kw))
+        db.op.add_column('resources', db.Column('cp', db.Integer, **kw))
+        db.op.add_column('resources', db.Column('host', db.String(255), **kw))
+        db.op.add_column('resources', db.Column('mem', db.Integer, **kw))
+        db.reflect()
+        yield
+
+    shutil.rmtree(tempdir)
--- tests/test_query.py
+++ tests/test_query.py
@@ -4,26 +4,13 @@ import pytest
 
 from oar.lib.models import all_models, all_tables
 from oar.lib.fixture import load_fixtures
-
+from oar.lib import db
 
 REFTIME = 1437050120
 
 
-@pytest.fixture(scope='module', autouse=True)
-def setp_db():
-    from oar.lib import db
-    kwargs = {"nullable": True}
-    db.create_all()
-    db.op.add_column('resources', db.Column('core', db.Integer, **kwargs))
-    db.op.add_column('resources', db.Column('cp', db.Integer, **kwargs))
-    db.op.add_column('resources', db.Column('host', db.String(255), **kwargs))
-    db.op.add_column('resources', db.Column('mem', db.Integer, **kwargs))
-
-
 @pytest.fixture(autouse=True)
-def db(request):
-    from oar.lib import db
-
+def setup_db(request):
     # populate database
     here = os.path.abspath(os.path.dirname(__file__))
     filename = os.path.join(here, "data", "dataset_1.json")
@@ -33,14 +20,8 @@ def db(request):
     db.session.expunge_all()
     db.session.commit()
 
-    def teardown():
-        db.delete_all()
 
-    request.addfinalizer(teardown)
-    return db
-
-
-def test_simple_models(db):
+def test_simple_models():
     expected_models = [
         'Accounting', 'AdmissionRule', 'AssignedResource', 'Challenge',
         'EventLog', 'EventLogHostname', 'File', 'FragJob',
@@ -58,7 +39,7 @@ def test_simple_models(db):
     assert len(dict(all_tables()).keys()) == len(expected_models) + 1
 
 
-def test_get_jobs_for_user_query(db):
+def test_get_jobs_for_user_query():
     jobs = db.queries.get_jobs_for_user("user1").all()
     assert len(jobs) == 2
     assert jobs[0].id == 5
