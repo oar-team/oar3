d4c16dc8e5825110174e28014c9666a514910d72
Salem Harrache <salem.harrache@inria.fr>
2015-10-02 14:29:16 +0200

    [tests] Configured test with multiple database (pgsql et sqlite)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,59 @@
 # coding: utf-8
 from __future__ import unicode_literals, print_function
-import pytest
-from tempfile import mkstemp
-from oar.lib import config
 
+import os
+import tempfile
+import shutil
+import pytest
+
+from codecs import open
+
+from oar.lib import config, db
+from oar.lib.compat import iteritems
 from . import DEFAULT_CONFIG
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def setup_config(request):
     config.clear()
     config.update(DEFAULT_CONFIG.copy())
-    _, config["LOG_FILE"] = mkstemp()
+    tempdir = tempfile.mkdtemp()
+    config["OAR_DIR"] = tempfile.mkdtemp()
+    config["LOG_FILE"] = os.path.join(config["OAR_DIR"], 'oar.log')
+    config["CONFIG_FILE"] = os.path.join(config["OAR_DIR"], 'oar.conf')
+
+    db_type = os.environ.get('DB_TYPE', 'memory')
+
+    if db_type not in ('memory', 'sqlite', 'postgresql'):
+        raise ValueError("Unsupported database '%s'" % db_type)
+
+    if db_type == "sqlite":
+        config['DB_BASE_FILE'] = os.path.join(config["OAR_DIR"], 'db.sqlite')
+        config['DB_TYPE'] = 'sqlite'
+    elif db_type == "memory":
+        config['DB_TYPE'] = 'sqlite'
+        config['DB_BASE_FILE'] = ':memory:'
+    else:
+        config['DB_TYPE'] = 'Pg'
+        config['DB_PORT'] = '5432'
+        config['DB_BASE_NAME'] = 'oar'
+        config['DB_BASE_PASSWD'] = 'oar'
+        config['DB_BASE_LOGIN'] = 'oar'
+        config['DB_BASE_PASSWD_RO'] = 'oar'
+        config['DB_BASE_LOGIN_RO'] = 'oar'
+        config['DB_HOSTNAME'] = 'localhost'
+
+    def dump_configuration(filename):
+        with open(filename, 'w', encoding='utf-8') as fd:
+            for key, value in iteritems(config):
+                if not key.startswith('SQLALCHEMY_'):
+                    fd.write("%s=%s\n" % (key, str(value)))
+
+    @request.addfinalizer
+    def teardown():
+        shutil.rmtree(tempdir)
+
+    dump_configuration(config["CONFIG_FILE"])
+    db.create_all()
+    db.reflect()
+    db.delete_all()
