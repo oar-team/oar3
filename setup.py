cbf1fd54956440115cbae1d3ce84a5bd348d41d9
Salem Harrache <salem.harrache@inria.fr>
2015-10-02 12:01:50 +0200

    updated setuptool install script

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
 import re
 import os.path as op
+
+
+from codecs import open
 from setuptools import setup, find_packages
 
-here = op.abspath(op.dirname(__file__))
+
+def read(fname):
+    ''' Return the file content. '''
+    here = op.abspath(op.dirname(__file__))
+    with open(op.join(here, fname), 'r', 'utf-8') as fd:
+        return fd.read()
+
+readme = read('README.rst')
+changelog = read('CHANGES.rst').replace('.. :changelog:', '')
 
 requirements = [
     'oar-lib',
@@ -10,30 +24,33 @@ requirements = [
     'click',
 ]
 
+if sys.version_info[0] == 2:
+    # TODO: put python2-only package requirements
+    # requirements.append('example-package')
+    pass
 
-def read(fname):
-    ''' Return the file content. '''
-    with open(op.join(here, fname)) as fd:
-        return fd.read()
+version = ''
+version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                    read(op.join('oar_cli', '__init__.py')),
+                    re.MULTILINE).group(1)
 
-
-def get_version():
-    return re.compile(r".*__version__ = '(.*?)'", re.S)\
-             .match(read(op.join(here, 'oar_cli', '__init__.py'))).group(1)
+if not version:
+    raise RuntimeError('Cannot find version information')
 
 
 setup(
     name='oar-cli',
-    author='Salem Harrache',
+    author="Salem Harrache",
     author_email='salem.harrache@inria.fr',
-    version=get_version(),
+    version=version,
     url='https://github.com/oar-team/oar-cli',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
     zip_safe=False,
-    description='OAR Command line interface.',
-    long_description=read('README.rst') + '\n\n' + read('CHANGES.rst'),
+    description="OAR 3 Command line interface",
+    long_description=readme + '\n\n' + changelog,
+    keywords='oar-cli',
     license='BSD',
     classifiers=[
         'Development Status :: 4 - Beta',
