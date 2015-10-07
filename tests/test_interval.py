0118eb963946f3266788bd1948e090fde519e22d
Olivier Richard <olivier.richard@imag.fr>
2015-10-07 14:27:50 +0200

    [tests] Added tests for interval

--- tests/test_interval.py
+++ tests/test_interval.py
@@ -1,10 +1,10 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 from oar.lib.interval import (intersec, extract_n_scattered_block_itv,
                               ordered_ids2itvs, itvs2ids, add_intervals,
                               equal_itvs, equal_and_sub_prefix_itvs,
                               equal_itvs2ids, equal_itvs_same_segmentation,
-                              aggregate_itvs)
+                              aggregate_itvs, itvs_size, sub_intervals, unordered_ids2itvs)
 
 
 def test_intersec():
@@ -29,6 +29,13 @@ def test_ordered_ids2itvs():
     y = [1, 3, 4, 5, 7, 10, 11, 12, 23]
     r = [(1, 1), (3, 5), (7, 7), (10, 12), (23, 23)]
     a = ordered_ids2itvs(y)
+    assert a == r
+
+
+def test_unordered_ids2itvs():
+    y = [12, 23, 7, 5, 4, 11, 10, 1, 3]
+    r = [(1, 1), (3, 5), (7, 7), (10, 12), (23, 23)]
+    a = unordered_ids2itvs(y)
     assert a == r
 
 
@@ -61,6 +68,16 @@ def test_add_intervals3():
     y = [(1, 10), (15, 20), (22, 24)]
     a = add_intervals(x, y)
     assert a == r
+
+
+def test_add_intervals4():
+    r = [(1, 30)]
+    assert add_intervals(r, []) == r
+
+
+def test_add_intervals5():
+    r = [(1, 30)]
+    assert add_intervals([], r) == r
 
 
 def test_equal_itvs1():
@@ -109,7 +126,29 @@ def test_equal_itvs_same_segmentation_3():
     assert(not equal_itvs_same_segmentation([(2, 4), (6, 9), (10, 14)], [(2, 4), (6, 14)]))
 
 
-def test_aggregate_itvs():
+def test_aggregate_itvs_1():
     r = [(1, 2), (4, 10)]
     a = aggregate_itvs([(1, 2), (4, 5), (6, 6), (7, 10)])
     assert a == r
+
+
+def test_aggregate_itvs_2():
+    assert aggregate_itvs([]) == []
+
+
+def test_itvs_size():
+    assert itvs_size([(1, 2), (4, 5), (10, 10)]) == 5
+
+
+def test_subintervals_1():
+    x = [(1, 10)]
+    y = [(2, 5)]
+    r = [(1, 1), (6, 10)]
+    assert sub_intervals(x, y) == r
+
+
+def test_subintervals_2():
+    x = [(1, 10), (20, 30)]
+    y = [(2, 5), (24, 27)]
+    r = [(1, 1), (6, 10), (20, 23), (28, 30)]
+    assert sub_intervals(x, y) == r
