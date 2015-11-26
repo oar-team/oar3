ace1d5caf0658f248f61efa1c0d5601ae531a521
Salem Harrache <salem.harrache@inria.fr>
2015-11-26 11:09:25 +0100

    Fixed collected tests

--- tests/data/populate.py
+++ tests/data/populate.py
@@ -4,7 +4,8 @@ import time
 from oar.lib import db, load_fixtures
 
 
-# populate database
-here = os.path.abspath(os.path.dirname(__file__))
-filename = os.path.join(here, "dataset_1.json")
-load_fixtures(db, filename, ref_time=int(time.time()), clear=True)
+if __name__ == '__main__':
+    # populate database
+    here = os.path.abspath(os.path.dirname(__file__))
+    filename = os.path.join(here, "dataset_1.json")
+    load_fixtures(db, filename, ref_time=int(time.time()), clear=True)
