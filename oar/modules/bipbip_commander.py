ed951782b92424fc71f49e02da21ddd550505f5a
Olivier Richard <olivier.richard@imag.fr>
2016-07-26 17:15:22 +0200

    [Modules] wip: bipbip_commander

--- oar/modules/bipbip_commander.py
+++ oar/modules/bipbip_commander.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 """Process that launches and manages bipbip processes
-   OAREXEC_REGEXP   'OAREXEC_(\d+)_(\d+)_(\d+|N)_(\d+)'
-   OARRUNJOB_REGEXP   'OARRUNJOB_(\d+)';
-   LEONEXTERMINATE_REGEXP   'LEONEXTERMINATE_(\d+)'
-"""
+   OAREXEC_REGEXP   OAREXEC_(\d+)_(\d+)_(\d+|N)_(\d+)
+   OARRUNJOB_REGEXP   OARRUNJOB_(\d+)
+   LEONEXTERMINATE_REGEXP   'LEONEXTERMINATE_(\d+)
 
-import time
+   Commands:
+     OAREXEC
+     OARRUNJOB
+     LEONEXTERMINATE
+
+
+   Example
+{
+   "job_id": 5,
+   "cmd": "LEONEXTERMINATE"
+   "args": [5]
+}
+
+"""
+from multiprocessing import Process
+
 import socket
 import zmq
 
-from  multiprocessing import Process
 
 from oar.lib import (config, get_logger)
 from oar.lib.tools import call
@@ -30,7 +43,7 @@ DEFAULT_CONFIG = {
 config.setdefault_config(DEFAULT_CONFIG)
 
 # Max number of concurrent bipbip processes
-Max_bipbip_processes = config['MAX_CONCURRENT_JOBS_STARTING_OR_TERMINATING']
+Max_bipbip_processes = int(config['MAX_CONCURRENT_JOBS_STARTING_OR_TERMINATING'])
 Detach_oarexec = config['DETACH_JOB_FROM_SERVER']
 
 # Maximum duration a a bipbip process (after that time the process is killed)
@@ -40,64 +53,72 @@ logger = get_logger("oar.modules.bipbip_commander", forward_stderr=True)
 logger.info('Start Bipbip Commander')
 
 
-def bipbip_leon_executor(command, args):
-    logger.debug('[bipbip_commander] Launching' + command['cmd'])
-    call(command["cmd"].split(), shell=True)
+class BipbipCommander(object):
+    
+    def __init__(self):
+        # Initialize a zeromq context
+        self.context = zmq.Context()
+        self.appendice = self.context.socket(zmq.PUSH) # to signal Almighty
+        self.appendice.connect('tcp://' + config['SERVER_HOSTNAME'] + ':' + config['ZMQ_SERVER_PORT'])
 
 
-def main():
-    # Initialize a zeromq context
-    context = zmq.Context()
-    appendice = context.socket(zmq.PUSH) # signal Almighty
-    appendice.connect('tcp://' + config['SERVER_HOSTNAME'] + ':' + config['ZMQ_SERVER_PORT'])
+        # IP addr is required when bind function is used on zmq socket
+        ip_addr_bipbip_commander = socket.gethostbyname(config['BIPBIP_COMMANDER_SERVER'])
+        self.notification = self.context.socket(zmq.PULL) # receive zmq formatted OAREXEC / OARRUNJOB / LEONEXTERMINATE
+        self.notification.bind('tcp://' + ip_addr_bipbip_commander + ':' + config['BIPBIP_COMMANDER_PORT'])
+        
+        self.bipbip_leon_commands_to_run = []
+        self.bipbip_leon_commands_to_requeue = []
+        self.bipbip_leon_executors = {}
 
+    def bipbip_leon_executor(*args, **command):
+            logger.debug('[bipbip_commander] Launching' + str(command))
+            print('[bipbip_commander] Launching', command['cmd'], command['args'])
+            #call(command["cmd"].split(), shell=True)
 
-    # IP addr is required when bind function is used on zmq socket
-    ip_addr_bipbip_commander = socket.gethostbyname(config['BIPBIP_COMMANDER_SERVER'])
-    notification = context.socket(zmq.PULL) # receive zmq formatted OAREXEC / OARRUNJOB / LEONEXTERMINATE
-    notification.bind('tcp://' + ip_addr_bipbip_commander + ':' + config['BIPBIP_COMMANDER_PORT'])
+    
+    def run(self, loop=True):
+        # TODO: add a shutdown procedure
+        while True:
+            #add_timeout if bipbip_leon_commands_to_run is not empty
+            command = self.notification.recv_json()
 
-    bipbip_leon_commands_to_run = []
-    bipbip_leon_commands_to_requeue = []
-    bipbip_leon_executors = {}
+            logger.debug("bipbip commander received notification:" + str(command))
 
-    # TODO: add a shutdown procedure
-    while True:
-        #add_timeout if bipbip_leon_commands_to_run is not empty
-        command = notification.recv_json()
-        current = time.time()
-        logger.debug("bipbip commander received notification:" + str(command))
+            self.bipbip_leon_commands_to_run.append(command)
 
-        bipbip_leon_commands_to_run.append(command)
+            while len(self.bipbip_leon_commands_to_run) > 0 and \
+                  len(self.bipbip_leon_executors.keys()) <= Max_bipbip_processes:
 
-        while len(bipbip_leon_commands_to_run) > 0 and \
-              len(bipbip_leon_executors.keys()) <= Max_bipbip_processes:
+                command = self.bipbip_leon_commands_to_run.pop(0)
+                job_id = command['job_id']
+                flag_exec = True
 
-            command = bipbip_leon_commands_to_run.pop(0)
-            job_id = command['job_id']
-            flag_exec = True
+                if job_id in self.bipbip_leon_executors:
+                    if not self.bipbip_leon_executors[job_id].is_alive:
+                        del self.bipbip_leon_executors[job_id]
+                    else:
+                        flag_exec = False
+                        # requeue command
+                        logger.debug("[bipbip_commander] A process is already running for the job " +
+                                     str(job_id) + ". We requeue: " + str(command))
+                        self.bipbip_leon_commands_to_requeue.append(command)
 
-            if job_id in bipbip_leon_executors:
-                if not bipbip_leon_executors[job_id].is_alive:
-                    del bipbip_leon_executors[job_id]
-                else:
-                    flag_exec = False
-                    #requeue command
-                    logger.debug("[bipbip_commander] A process is already running for the job " +
-                                 job_id + ". We requeue: " + str(command))
-                    bipbip_leon_commands_to_requeue.append(command)
+                if flag_exec:
+                    # exec
+                    executor = Process(target=self.bipbip_leon_executor, args=(), kwargs=command)
+                    executor.start()
+                    self.bipbip_leon_executors[job_id] = executor
 
-            if flag_exec:
-                #exec
-                executor = Process(target=bipbip_leon_executor, args=(command))
-                executor.start()
-                bipbip_leon_executors[job_id] = executor
+            # append commands to requeue
+            self.bipbip_leon_commands_to_run += self.bipbip_leon_commands_to_requeue
+            self.bipbip_leon_commands_to_requeue = []
 
-        # append commands to requeue
-        bipbip_leon_commands_to_run += bipbip_leon_commands_to_requeue
-        bipbip_leon_commands_to_requeue = []
+            if not loop:
+                break
 
 
 if __name__ == "__main__":
-    main()
+    bipbip_commander = BipbipCommander()
+    bipbip_commander.run()
 
