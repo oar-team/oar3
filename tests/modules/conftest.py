83c2b9ee9fd3c9e98c27bc1da392b3cc16e884a6
Olivier Auguste Richard <olivier.richard@imag.fr>
2018-02-01 16:08:10 +0100

    [tests/modules] Add conftest.py

---
+++ tests/modules/conftest.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+import oar.lib.tools  # for monkeypatching
+
+from oar.lib import (db, Resource, Queue) 
+
+# @pytest.yield_fixture(scope='function', autouse=True)
+# def minimal_db_initialization(request):
+#     with db.session(ephemeral=True):
+#         # add some resources
+#         for i in range(5):
+#             Resource.create(network_address='localhost'+str(i))
+#         Queue.create(name='default')
+#         db.commit()
+#         yield
