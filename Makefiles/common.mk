8cb4e2a668c2fab35e4d9a4c6ce6162a1e5ad5fe
Olivier Richard <olivier.richard@imag.fr>
2018-03-30 16:18:29 +0200

    [Makefiles] update common.mk

--- Makefiles/common.mk
+++ Makefiles/common.mk
@@ -1,14 +1,9 @@
 MODULE=common
-SRCDIR=sources/core
+SRCDIR=oar
 
 OARDIR_BINFILES = $(SRCDIR)/tools/oarsh/oarsh_shell.in \
-	          $(SRCDIR)/tools/oarsh/oarsh.in \
-                  $(SRCDIR)/qfunctions/oarnodesetting \
-		  $(SRCDIR)/tools/sentinelle.pl
-
-MANDIR_FILES = $(SRCDIR)/man/man1/oarsh.1 \
-	       $(SRCDIR)/man/man1/oarprint.1 \
-	       $(SRCDIR)/man/man1/oarnodesetting.1
+	              $(SRCDIR)/tools/oarsh/oarsh.in \
+		          $(SRCDIR)/tools/sentinelle.pl
 
 SHAREDIR_FILES = $(SRCDIR)/tools/oar.conf.in \
                    $(SRCDIR)/tools/oarnodesetting_ssh.in \
@@ -20,43 +15,34 @@ PROCESS_TEMPLATE_FILES = $(SRCDIR)/tools/oarsh/oarcp.in \
 			 $(SRCDIR)/tools/oardodo.c.in \
 			 $(SRCDIR)/tools/oardo.c.in
 
-
-
 include Makefiles/shared/shared.mk
 
 clean: clean_shared
-	$(MAKE) -f Makefiles/man.mk clean
 	$(OARDO_CLEAN) CMD_WRAPPER=$(OARDIR)/oarsh CMD_TARGET=$(DESTDIR)$(BINDIR)/oarsh
 	$(OARDO_CLEAN) CMD_WRAPPER=$(OARDIR)/oarnodesetting CMD_TARGET=$(DESTDIR)$(SBINDIR)/oarnodesetting
 	-rm -f $(SRCDIR)/tools/oardodo
 
 build: build_shared
-	$(MAKE) -f Makefiles/man.mk build
 	$(OARDO_BUILD) CMD_WRAPPER=$(OARDIR)/oarsh CMD_TARGET=$(DESTDIR)$(BINDIR)/oarsh
 	$(OARDO_BUILD) CMD_WRAPPER=$(OARDIR)/oarnodesetting CMD_TARGET=$(DESTDIR)$(SBINDIR)/oarnodesetting
-	
+
 	$(CC) $(CPPFLAGS) $(CFLAGS) $(LDFLAGS) -o $(SRCDIR)/tools/oardodo $(SRCDIR)/tools/oardodo.c
-	
+
 install: install_shared
-	
 	$(OARDO_INSTALL) CMD_WRAPPER=$(OARDIR)/oarsh CMD_TARGET=$(DESTDIR)$(BINDIR)/oarsh
 	$(OARDO_INSTALL) CMD_WRAPPER=$(OARDIR)/oarnodesetting CMD_TARGET=$(DESTDIR)$(SBINDIR)/oarnodesetting
-	
+
 	install -d $(DESTDIR)$(BINDIR)
 	install -m 0755 $(SRCDIR)/tools/oarsh/oarcp $(DESTDIR)$(BINDIR)/
-	install -m 0755 $(SRCDIR)/qfunctions/oarprint $(DESTDIR)$(BINDIR)
-	
+    # install -m 0755 $(SRCDIR)/qfunctions/oarprint $(DESTDIR)$(BINDIR)
+
 	install -d $(DESTDIR)$(OARDIR)/oardodo
 	install -m 0754 $(SRCDIR)/tools/oardodo $(DESTDIR)$(OARDIR)/oardodo
-	
-	cp -f $(DESTDIR)$(MANDIR)/man1/oarsh.1 $(DESTDIR)$(MANDIR)/man1/oarcp.1
-	
+
 uninstall: uninstall_shared
 	$(OARDO_UNINSTALL) CMD_WRAPPER=$(OARDIR)/oarsh CMD_TARGET=$(DESTDIR)$(BINDIR)/oarsh
-	$(OARDO_UNINSTALL) CMD_WRAPPER=$(OARDIR)/oarnodesetting CMD_TARGET=$(DESTDIR)$(SBINDIR)/oarnodesetting
-	rm -f $(DESTDIR)$(MANDIR)/man1/oarcp.1
+    $(OARDO_UNINSTALL) CMD_WRAPPER=$(OARDIR)/oarnodesetting CMD_TARGET=$(DESTDIR)$(SBINDIR)/oarnodesetting
 	rm -rf $(DESTDIR)$(OARDIR)/oardodo
 	rm -rf $(DESTDIR)$(EXAMPLEDIR)
-	
 
 .PHONY: install setup uninstall build clean
