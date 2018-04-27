8e714ba8b08884af1d54570e4429fd31b6292b08
Olivier Richard <olivier.richard@imag.fr>
2018-04-27 15:22:49 +0200

    [tests/live] add live oarsub -I test

--- tests/live/test_basic.py
+++ tests/live/test_basic.py
@@ -6,6 +6,8 @@
 #
 import pytest
 import re
+import pexpect
+from subprocess import call
 from . import live_testing, pytestmark
 
 @live_testing
@@ -15,3 +17,15 @@ def test_oarsub_date(script_runner):
     assert re.match(r'^OAR_JOB_ID= \d+$', ret.stdout)
     assert ret.success
 
+@live_testing
+def test_oarsub_I():
+    child = pexpect.spawn('oarsub -I')
+    try: 
+        child.expect('OAR_JOB_ID= (\d+)\r\n', timeout=30)
+        job_id = child.match.group(1).decode()
+        call('oardel ' + job_id, shell=True)
+        child.expect(pexpect.EOF, timeout=30)
+    except pexpect.TIMEOUT:
+        assert False
+    assert not child.isalive()
+    assert child.exitstatus == 0
