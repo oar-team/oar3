e0a579265ad8fbec9efb1e8dbc68d35ba090e48b
Olivier Richard <olivier.richard@imag.fr>
2018-07-18 11:01:56 +0200

    [tests/hulot] Fix test for WindowForker

--- tests/modules/test_hulot.py
+++ tests/modules/test_hulot.py
@@ -268,12 +268,14 @@ def test_hulot_command_executor(monkeypatch):
 def yop(a):
     return a
 def test_hulot_window_forker_check_executors():
-
     wf = WindowForker(1, 10)
     wf.executors = { wf.pool.apply_async(yop, (0,)): ('node1', 'HALT', tools.get_date()),
                      wf.pool.apply_async(yop, (0,)): ('node2', 'WAKEUP', tools.get_date())}
     nodes_list_running = {'node1': 'command_and_args', 'node2': 'command_and_args'}
-    wf.check_executors(nodes_list_running)
+    while True:
+        wf.check_executors(nodes_list_running)
+        if len(wf.executors) != 2:
+            break
     print(nodes_list_running)
     assert nodes_list_running ==  {'node2': 'command_and_args'}
     
