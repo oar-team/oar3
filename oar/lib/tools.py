f8104a677263467d97476977f642b504dfdb8ba6
Olivier Richard <olivier.richard@imag.fr>
2020-02-18 22:48:19 +0100

    [tools] fix quote escaping in limited_dict2hash_perl function

--- oar/lib/tools.py
+++ oar/lib/tools.py
@@ -690,7 +690,7 @@ def limited_dict2hash_perl(d):
             else:
                 s = s + limited_dict2hash_perl(v)
         elif isinstance(v, str):
-            s = s + "'" + v + "'"
+            s = s + "'" + v.replace("'", "\\'") + "'"
         elif isinstance(v, bool):
             val = '1' if v else '0'
             s = s + val
