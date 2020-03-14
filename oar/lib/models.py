3494744797b76ce4cc440ac4ea998b5d3dca858c
Olivier Richard <olivier.richard@imag.fr>
2020-03-14 14:04:06 +0100

    [model] add WalltimeChange table

--- oar/lib/models.py
+++ oar/lib/models.py
@@ -326,6 +326,19 @@ class Scheduler(db.Model):
     script = db.Column(db.String(100))
     description = db.Column(db.String(255))
 
+class WalltimeChange(db.Model):
+    __tablename__ = 'walltime_change'
+
+    job_id = db.Column(db.Integer, primary_key=True, autoincrement=False, index=True, server_default='0')
+    pending = db.Column(db.Integer, server_default='0')
+    force = db.Column(db.String(3), index=True, server_default='NO')
+    delay_next_jobs = db.Column(db.String(3), index=True, server_default='NO')
+    granted = db.Column(db.Integer, server_default='0')
+    granted_with_force = db.Column(db.Integer, server_default='0')
+    granted_with_delay_next_jobs = db.Column(db.Integer, server_default='0')
+
+    __table_args__ = (db.CheckConstraint(force.in_(['NO', 'YES'])),
+                      db.CheckConstraint(delay_next_jobs .in_(['NO', 'YES'])))
 
 def all_models():
     for name, obj in inspect.getmembers(sys.modules[__name__]):
