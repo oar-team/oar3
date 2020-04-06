1ce37ce895a2f994ebe46642fec2d78cfdeb4343
Olivier Richard <olivier.richard@imag.fr>
2020-04-06 16:22:59 +0200

    [kao/quotas] add Calendar class to support temporal quotas

--- oar/kao/quotas.py
+++ oar/kao/quotas.py
@@ -3,26 +3,206 @@ import simplejson as json
 from collections import defaultdict
 from copy import deepcopy
 
-from crontab import CronTab
-
-from oar.lib import config
+from datetime import (date, datetime, timedelta)
+   
+from oar.lib import (config, get_logger)
+from oar.lib.submission import check_reservation
+from oar.lib.tools import hms_str_to_duration
 
 import oar.lib.resource as rs
 
-class CrontabQuotas(object):
-    def __init__(self, crontab_quotas_str):
-        crontab_str, self.quotas_name, self.description = crontab_quotas_str
-        self.crontab = Crontab(crontab_str)
+_day2week_offset = {'mon': 0, 'tue': 1, 'wed': 2, 'thu': 3, 'fri': 4, 'sat': 5, 'sun': 6}
+
+logger = get_logger('oar.kao.quotas')
+
+class Calendar(object):
+
+    def __init__(self, json_quotas):
+        self.quotas_period = config['QUOTAS_PERIOD']
+        self.period_end = 0 # period_end = period_begin = quotas_period
         
-class CrontabQuotasList(object):
-    crontab_list = {}
-    def __init__(self, crontab_list_str):
-        self.crontabquotas_list = crontabq_list
-        i=0
-        self.ct2idx = {}
-        for ct in self.crontab_list:
-            crontab, quotas_name, descrition = ct
-            #if quotas_name in
+        self.ordered_periodical_ids = []
+        self.op_index = 0
+        self.periodicals = {} 
+        self.nb_periodicals = 0
+         
+        self.ordered_oneshot_ids = []
+        self.oneshots = {}
+        self.oneshots_begin = None
+        self.oneshots_end = None
+        self.nb_oneshots = 0
+        
+        self.quotas_rules_list = []
+        self.quotas_rules2id = {}
+        self.quotas_id2rules = {}
+        self.nb_quotas_rules = 0
+
+        # load of quotas rules sets
+        for type_temporal_quotas_i in [('periodical', 1), ('oneshot', 2)]:
+            type_temporal_quotas, i = type_temporal_quotas_i 
+            if type_temporal_quotas in json_quotas:
+                for p in json_quotas[type_temporal_quotas]:
+                    if p[i] not in self.quotas_rules2id:
+                        self.quotas_rules_list.append(Quotas.quotas_rules_fromJson(json_quotas[p[i]]))
+                        self.quotas_rules2id[p[i]] = self.nb_quotas_rules
+                        self.quotas_id2rules[self.nb_quotas_rules] = p[i]
+                        self.nb_quotas_rules += 1
+
+        # create periodicals and oneshots data structure from json
+        if 'periodical' in json_quotas:
+            for p in json_quotas['periodical']:
+                self.periodical_fromJson(p)    
+            self.ordered_periodical_ids = sorted(range(self.nb_periodicals),
+                                                 key=lambda k: self.periodicals[k][0])
+        if 'oneshot' in json_quotas:
+            for o in json_quotas['oneshot']:
+                self.oneshot_fromJson(o)    
+            self.ordered_oneshot_ids = sorted(range(self.nb_oneshots),
+                                                 key=lambda k: self.oneshots[k][0])
+        
+    def periodical_fromJson(self, periodical_json):
+        """fill periodical data structures from one json entry 
+        ex: ["00:00-09:00 mon * *", "quotas_weekend"]
+        """
+        hm2, wday, mday, month = periodical_json[0].split()
+        if (mday != '*') or (month != '*'):     
+            logger.error('mday and month are not yet implemented: {}'.format(periodical_json[0]))
+            exit(1)
+            
+        try:
+            # handle hour, minute range
+            if hm2 == '*':
+                 begin = 0
+                 duration = 24*3600
+            else:
+                hm0, hm1 = hm2.split('-')
+                begin = hms_str_to_duration(hm0)
+                end = hms_str_to_duration(hm1)
+                if end:
+                    duration = end - begin
+                else: # end = 00:00
+                    duration = 24*3600 - begin
+
+            # handle days
+            if wday == '*': # all weekdays
+                weekdays = range(7)
+            elif '-' in wday: # days interval
+                d0, d1 = wday.split('-')
+                d0 = _day2week_offset[d0]
+                d1 = _day2week_offset[d1]
+                if hm2 == '*':
+                    if d0 > d1:
+                        d1 += 7
+                    weekdays = [d0]
+                    duration = 86400 * (d1-d0+1)
+                else:    
+                    weekdays = range(d0, d1+1)
+            else: # some days
+                weekdays = [_day2week_offset[d] for d in wday.split(',')]
+
+            # fill periodicals (begin, duration, rules_id)
+            for i in weekdays:
+                t0 = 86400*i + begin 
+                #overflow case: ex ["* sun-mon * *", "quotas_weekend", "weekend"],
+                if (t0 + duration) > 7*86400: 
+                    duration0 = (t0 + duration) % (7*86400)
+                    duration -= duration0
+                    self.periodicals[self.nb_periodicals] = (0, duration0,
+                                                             self.quotas_rules2id[periodical_json[1]],
+                                                             periodical_json[0])
+                    self.nb_periodicals += 1
+                self.periodicals[self.nb_periodicals] = (t0, duration,
+                                                         self.quotas_rules2id[periodical_json[1]],
+                                                         periodical_json[0])
+                self.nb_periodicals += 1
+                
+        except Exception as e:
+                logger.error('Parsing {} failed: {}'.format(periodical_json[0],e))
+                exit(1)
+        
+    def oneshot_fromJson(self, oneshot_json):
+        """fill oneshot data structures from one json entry 
+        ex: ["2020-07-23 19:30", "2012-08-29 8:30", "quotas_holyday", "summer holiday"]
+        """
+        error0, d0 = check_reservation(oneshot_json[0] + ':00')
+        error1, d1 = check_reservation(oneshot_json[1] + ':00')
+
+        if error0[0] or error1[0]:
+            logger.error('Parsing error on {} or/and {}, expected format is: "YYYY-MM-DD hh:mm"'\
+                         .format(d0, d1))
+            exit(1)
+        self.oneshots[self.nb_oneshots] = (d0, d1-d0, self.quotas_rules2id[oneshot_json[2]],
+                                           oneshot_json[3])
+        self.nb_oneshots += 1
+
+    def check_periodicals(self):
+        t = 0
+        for i in self.ordered_periodical_ids:
+            periodical = self.periodicals[i]
+            if periodical[0] != t:
+                return(False, i)
+            t = periodical[0] + periodical[1]
+        if t != 7 * 24 * 3600:
+            return(False, i)
+        return(True, None)
+        
+    def rules_at(self, t_epoch):
+        # TODO take into account oneshot case
+        t_dt = datetime.fromtimestamp(t_epoch).date()
+        # determine weekstart's day
+        t_weekstart_day_dt = t_dt - timedelta(days=t_dt.weekday())
+        period_origin = int(datetime.combine(t_weekstart_day_dt, datetime.min.time()).timestamp())
+
+        self.period_end = period_origin + self.quotas_period
+
+        # find rules_id and compute remaining_duration
+        for i in range(self.nb_periodicals):
+            t = t_epoch - period_origin
+            t_periodicals_begin = self.periodicals[self.ordered_periodical_ids[i]][0]
+            t_periodicals_end = t_periodicals_begin\
+                                + self.periodicals[self.ordered_periodical_ids[i]][1]-1
+            if (t_periodicals_begin >= t) and (t <= t_periodicals_end):
+                break
+
+        # get rules_id and compute remaining_duration 
+        self.op_index = i
+        index = self.ordered_periodical_ids[self.op_index]
+        rules_id =  self.periodicals[index][2]
+        remaining_duration = self.periodicals[index][1] - (t_epoch - period_origin)
+        
+        return (rules_id, remaining_duration)
+
+    def next_rules(self, t_epoch):
+        # TODO take into account oneshot case
+        if t_epoch >= self.period_end:
+            rules_id = None
+            remaining_duration = 0
+        else:
+            self.op_index = (self.op_index + 1) % self.nb_periodicals
+            index = self.ordered_periodical_ids[self.op_index]
+            rules_id =  self.periodicals[index][2]
+            remaining_duration = self.periodicals[index][1]
+            
+        return (rules_id, remaining_duration)
+    
+    def show(self, begin=None, end=None, json=False):
+
+        for i in self.ordered_periodical_ids:
+            b, d, rules_id, period = self.periodicals[i]
+            print('{:>3}: ({:>6}, {:>6}, {:<23}, {:>2}, {:<15})'.format(i, b, d, period, rules_id,
+                                                                 self.quotas_id2rules[rules_id]))
+
+        check, p_id = self.check_periodicals()
+        if not check:
+            print('Periodical issue around: {}'.format(p_id))
+
+        print()
+
+        for i in self.ordered_oneshot_ids:
+            b, e, rules_id, description = self.oneshots[i]
+            print('{:>3}: ({:>10}, {:>10}, {:>2}, {:<15}, {:<20})'.format(i, b, e, rules_id,
+                                                                          self.quotas_id2rules[rules_id],
+                                                                          description))
             
 class Quotas(object):
     """
@@ -129,10 +309,9 @@ account (but the inner jobs are used to compute the quotas).
     """
 
     enabled = False
-    temporal = False
+    calendar = None
     rules = {}
     job_types = ['*']
-    crontabs = None
     
     @classmethod
     def enable(cls):
@@ -266,11 +445,19 @@ account (but the inner jobs are used to compute the quotas).
         # print('slots b e :' + str(slots[sid_left].b) + " " + str(slots[sid_right].e))
         slots_quotas.update(job, job_nb_resources, duration)
         return slots_quotas.check(job)
-    
+
+    @staticmethod
+    def quotas_rules_fromJson(json_quotas_rules):
+        rules = {}
+        for k, v in json_quotas_rules.items():
+            rules[tuple(k.split(','))] = [v[0], v[1], int(3600 * v[2])]
+        return rules
+
     @classmethod
     def load_quotas_rules(cls):
         """
-        Simple exemple
+        Simple example
+        --------------
     
         {
             "quotas": {
@@ -280,47 +467,41 @@ account (but the inner jobs are used to compute the quotas).
             "job_types": ['besteffort','deploy','console']
         }
     
-        Temportal quotas exemple
-    
+        Temporal example
+        ----------------
         {
-            "crontab": [
-                ["* 9-18 MON-FRI * *", "quotas_workdays", "workdays"],
-                ["* 19-23 MON-THU * *", "quotas_nigths", "nights of workdays"],
-                ["* 0-8 TUE-FRI * *", "quotas_nigths", "nights of workdays"],
-                ["* 19-32 FRI * *", "quotas_weekends", "weekend"],
-                ["* * * SAT-SUN * *", "quotas_weekends", "weekend"],
-                ["* 0-8 MON * *", "quotas_weekends ,", "weekend"]
-            ],
-            "quotas_workdays": {
-                "*,*,*,john": [100,-1,-1],
-                "*,projA,*,*": [200,-1,-1]
-            },
-            "quotas_nigths": {
-                "*,*,*,john": [100,-1,-1],
-                "*,projA,*,*": [200,-1,-1]
-            },
-            "quotas_weekends": {
-                "*,*,*,john": [100,-1,-1],
-                "*,projA,*,*": [200,-1,-1]
+            periodical": [
+               ["00:00-09:00 mon * *", "quotas_weekend"]
+               ["09:00-19:00 mon-fri * *", "quotas_workday"],
+               ["19:00-00:00 mon-thu * *", "quotas_nigth"],
+               ["00:00-08:00 tue-fri * *", "quotas_nigth"],
+               ["19:00-00:00 fri * *", "quotas_weekend"],
+               ["* sat-sun * *", "quotas_weekend"],
+            ],         
+            oneshot": [
+               ["2020-07-23 19:30", "2012-08-29 8:30", "quotas_holiday"]
+            quotas_workdays": {
+               "*,*,*,john": [100,-1,-1],
+               "*,projA,*,*": [200,-1,-1]
+            ,
+            quotas_nigths": {
+               "*,*,*,john": [100,-1,-1],
+               "*,projA,*,*": [200,-1,-1]
+            ,
+            quotas_weekends": {
+               "*,*,*,john": [100,-1,-1],
+               "*,projA,*,*": [200,-1,-1]
             }
-        }
+       }        
         
-    
         """
         quotas_rules_filename = config['QUOTAS_FILE']
         with open(quotas_rules_filename) as json_file:
             json_quotas = json.load(json_file)
-            if 'crontab' in json_quotas:
-                pass
-                # quotas_crontab = CrontabList(json_quotas)
-                # for quotas_name in quotas_crontab.quotas_names:
-                #     qr = {}
-                #     for k, v in json_quotas['quotas'].items():
-                #         qr[tuple(k.split(','))] = [v[0], v[1], int(3600 * v[2])]
-                #     clsrules.append(qr)
-            else:
-                for k, v in json_quotas['quotas'].items():
-                    cls.rules[tuple(k.split(','))] = [v[0], v[1], int(3600 * v[2])]
+            if ('periodical' in json_quotas) or ('oneshot' in json_quotas):
+                cls.calendar = Calendar(json_quotas)
+            if 'quotas' in json_quotas:
+                cls.rules = cls.quotas_rules_fromJson(json_quotas['quotas'])
             if 'job_types' in json_quotas:
                 cls.job_types.extend(json_quotas['job_types'])
     
