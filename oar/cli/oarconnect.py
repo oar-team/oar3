1c996dbdf369f8c1063ac3eb68d5f831b650c880
Faure Adrien <adrien.faure2@gmail.com>
2021-04-23 16:35:18 +0200

    [cli/oarconnect] clean unused variables

--- oar/cli/oarconnect.py
+++ oar/cli/oarconnect.py
@@ -1,5 +1,3 @@
-import os
-
 import click
 
 from oar.cli.oarsub import connect_job
@@ -11,16 +9,6 @@ from .utils import CommandReturns
 @click.command()
 @click.argument("job_id", nargs=1, required=True, type=int)
 def cli(job_id):
-
     cmd_ret = CommandReturns()
-    binpath = ""
-    if "OARDIR" in os.environ:
-        binpath = os.environ["OARDIR"] + "/"
-    else:
-        cmd_ret.error("OARDIR environment variable is not defined.", 0, 1)
-        cmd_ret.exit()
-
     openssh_cmd = config["OPENSSH_CMD"]
-    ssh_timeout = int(config["OAR_SSH_CONNECTION_TIMEOUT"])
-
     exit(connect_job(job_id, 0, openssh_cmd, cmd_ret))
