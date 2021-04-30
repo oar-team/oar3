8c1ebaf2a013184e45c8e5d5e7a56e3374d289e7
Faure Adrien <adrien.faure2@gmail.com>
2021-04-30 11:39:52 +0200

    [cli/oarwalltime] type in documentation

--- oar/cli/oarwalltime.py
+++ oar/cli/oarwalltime.py
@@ -144,7 +144,7 @@ def cli(job_id, new_walltime, force, delay_next_jobs, version):
     The value is a new walltime absolute value (like passed to oarsub). If prefixed
     by +, the request is an increase of the walltime by the passed value. If
     prefixed by -, it is a decrease request. In that case the arguments need
-    to be escaped with -- to avoid to be parsed as a command-line option (e.g. oarwalltime -- 1234 -1:0:0).
+    to be escaped with -- to avoid it to be parsed as a command-line option (e.g. oarwalltime -- 1234 -1:0:0).
 
     The job must be running to request a walltime change.
     """
