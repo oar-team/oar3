b96fda23d9c8489569205f2adb514d6ed4be9e00
Olivier Richard <olivier.richard@imag.fr>
2021-11-13 09:09:57 +0100

    remove useless a db's initial row

--- setup/database/default_data.sql
+++ setup/database/default_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO queues (queue_name, priority, scheduler_policy) VALUES ('admin','10','fifo');
 INSERT INTO queues (queue_name, priority, scheduler_policy) VALUES ('default','2','fifo');
 INSERT INTO queues (queue_name, priority, scheduler_policy) VALUES ('besteffort','0','fifo');
-INSERT INTO gantt_jobs_predictions (moldable_job_id , start_time) VALUES ('0','0');
+/* INSERT INTO gantt_jobs_predictions (moldable_job_id , start_time) VALUES ('0','0'); */
