14bd02545d872db1f98e50e1bff3ad60c6b6bfb6
Adrien Faure <adrien.faure@protonmail.com>
2022-09-02 12:09:50 +0200

    [poetry] drop support python < 3.9

--- scripts/ci/run-tests-with-docker.sh
+++ scripts/ci/run-tests-with-docker.sh
@@ -4,7 +4,7 @@ export ROOT_PROJECT="$(git rev-parse --show-toplevel)"
 export COMPOSE_FILE=$ROOT_PROJECT/scripts/ci/docker/docker-compose.test.yml
 export COMPOSE_PROJECT_NAME="$(basename "$ROOT_PROJECT")_ci"
 
-export PYTHON_IMAGE=${PYTHON_IMAGE:-python:3.7}
+export PYTHON_IMAGE=${PYTHON_IMAGE:-python:3.9}
 export POSTGRES_IMAGE=${POSTGRES_IMAGE:-postgres:11}
 
 echo "-------------------------------------------------------------------------------"
