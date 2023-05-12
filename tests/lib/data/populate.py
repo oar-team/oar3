54f669af5414354f6c4136e4865a91a0deede0e4
Adrien Faure <adrien.faure@protonmail.com>
2023-05-12 14:50:53 +0200

    [oar/lib] remove lazy loader

--- tests/lib/data/populate.py
+++ tests/lib/data/populate.py
@@ -2,10 +2,10 @@
 import os
 import time
 
-from oar.lib import load_fixtures
+from oar.lib.fixture import load_fixtures
 
 if __name__ == "__main__":
     # populate database
     here = os.path.abspath(os.path.dirname(__file__))
     filename = os.path.join(here, "dataset_1.json")
-    load_fixtures(db, filename, ref_time=int(time.time()), clear=True)
+    # load_fixtures(db, filename, ref_time=int(time.time()), clear=True)
