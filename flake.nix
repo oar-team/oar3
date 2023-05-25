128983db554a72e5422d5488181403229a63baaf
Adrien Faure <adrien.faure@protonmail.com>
2023-05-25 16:16:06 +0200

    [flake] add c++ path to dev shell

--- flake.nix
+++ flake.nix
@@ -47,11 +47,12 @@
         defaultPackage = self.packages.${system}.${packageName};
 
         devShell = pkgs.mkShell {
+          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
           buildInputs = with pkgs; [
             # (poetry2nix.mkPoetryEnv { projectDir = self; })
             # Install the entry point and the plugins
+            # Which is not needed anymore bc the plugins are on a new repo
             # (poetry2nix.mkPoetryApplication { projectDir = self; })
-            
             python3Packages.sphinx_rtd_theme
             poetry
             postgresql
