bfb5ebdfd4b28052fdbb7a73b288efcc397dcb5a
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2023-12-22 15:36:14 +0100

    [api] re-enabled wsgi file as it may be needed by nixos-compose

--- Makefiles/api.mk
+++ Makefiles/api.mk
@@ -12,6 +12,7 @@ SHAREDIR_FILES = $(SRCDIR)/../setup/apache2/apache2.conf.in \
 		   $(SRCDIR)/../setup/uvicorn/main.py.in\
 		   $(SRCDIR)/../setup/uvicorn/log.ini.in\
 		   $(SRCDIR)/../setup/uvicorn/oarapi.service.in\
+		   $(SRCDIR)/../setup/apache2/oarapi.wsgi\
 		   $(SRCDIR)/tools/stress_factor.sh
 
 include Makefiles/shared/shared.mk
