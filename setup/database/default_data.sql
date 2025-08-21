3e88b4f49316b2582766f3e737b07cad9d2c73b6
Olivier Richard <olivier.richard@imag.fr>
2025-08-21 10:05:41 +0200

    [setup] set kamelot for defaul external scheduler

--- setup/database/default_data.sql
+++ setup/database/default_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO queues (queue_name, priority, scheduler_policy) VALUES ('admin','10','kamelot');
+INSERT INTO queues (queue_name, priority, scheduler_policy) VALUES ('admin','10','kamelot';
 INSERT INTO queues (queue_name, priority, scheduler_policy) VALUES ('default','2','kamelot');
 INSERT INTO queues (queue_name, priority, scheduler_policy) VALUES ('besteffort','0','kamelot');
 /* INSERT INTO gantt_jobs_predictions (moldable_job_id , start_time) VALUES ('0','0'); */
